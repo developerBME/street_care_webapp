96736651a5bfcc5a5f3ee707b45181648ac825de
aniket14thumar <aniket14.thumar@gmail.com>
2023-08-31 21:02:15 -0400

    Configured Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
-import logo from './logo.png';
-import './App.css';
+import logo from "./logo.png";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Use <code>feature</code> branch to push your changes / make
-                    pull request. Let's keep <code>main</code> branch clear for
-                    now.
-                </p>
-                <p>
-                    We will push to main on a feature basis, once we are done
-                    with homepage, we will push it to main and later on use main
-                    branch to deploy our app.
-                </p>
-                <p>
-                    {' '}
-                    Also if majority of developers are comfortable using Vite
-                    instead of CRA, you can replace this boilerplate app with a
-                    Vite App before starting to work on the project.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+  return (
+    <div className="bg-black text-white justify-center w-screen h-screen">
+      <img src={logo} alt="logo" />
+      <p className="text-xl p-3">
+        Use <code>feature</code> branch to push your changes / make pull
+        request. Let's keep <code>main</code> branch clear for now.
+      </p>
+      <p className="text-xl p-3">
+        We will push to main on a feature basis, once we are done with homepage,
+        we will push it to main and later on use main branch to deploy our app.
+      </p>
+      <p className="text-xl p-3">
+        {" "}
+        Also if majority of developers are comfortable using Vite instead of
+        CRA, you can replace this boilerplate app with a Vite App before
+        starting to work on the project.
+      </p>
+    </div>
+  );
 }
 
 export default App;
