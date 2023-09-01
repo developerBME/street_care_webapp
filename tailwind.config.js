3ac691a7a64ade91f3505dd7230a014ab8451829
aniket14thumar <aniket14.thumar@gmail.com>
2023-09-01 09:32:30 -0400

    Added Font Family Bricolage Grotesque and Inter to the tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      bricolage: ["Bricolage Grotesque", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
+    },
   },
   plugins: [],
 };
