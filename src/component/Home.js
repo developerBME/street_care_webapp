d84d08836311b60cdcc236776d70fd15b8261f00
Nirav Patel <nnirav.patel7@gmail.com>
2023-09-01 18:26:42 -0400

    home page update

--- src/component/Home.js
+++ src/component/Home.js
@@ -5,8 +5,15 @@ function Home() {
   return (
     <div>
       <Navbar />
-      <div className=" relative bg-gradient-to-tr from-white to-yellow-200 h-auto flex flex-col  items-center">
+      <div className=" relative bg-gradient-to-tr from-white to-yellow-100 h-auto flex flex-col  items-center">
         <div className="  container mt-40 rounded-2xl bg-white w-full h-[819px] pt-40 text-black"></div>
+        <div className="  container mt-8 rounded-2xl bg-white w-full h-[819px] pt-40 text-black"></div>
+
+        {/* Vishnu */}
+        <div className="  container mt-8 rounded-2xl bg-white w-full h-[819px] pt-40 text-black"></div>
+        <div className="  container mt-8 rounded-2xl bg-white w-full h-[819px] pt-40 text-black"></div>
+        <div className="  container mt-8 rounded-2xl bg-white w-full h-[819px] pt-40 text-black"></div>
+        {/* Aniket */}
         <div className="  container mt-8 rounded-2xl bg-white w-full h-[819px] pt-40 text-black"></div>
         <div className="  container mt-8 rounded-2xl bg-white w-full h-[819px] pt-40 text-black"></div>
         <div className="  container mt-8 rounded-2xl bg-white w-full h-[819px] pt-40 text-black"></div>
