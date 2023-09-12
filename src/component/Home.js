bbffb2042594ff29d63796a853690a7636c3aaff
meetv123 <meetvanani123@gmail.com>
2023-09-12 19:05:35 -0400

    Section is almost done

--- src/component/Home.js
+++ src/component/Home.js
@@ -8,6 +8,9 @@ import Success from "./Success";
 import News from "./News";
 import Map from "./Map";
 import Process from "./Process";
+import pic1 from "../images/pic1.png";
+import pic2 from "../images/pic2.png";
+
 
 
 function Home() {
@@ -52,8 +55,65 @@ function Home() {
         </div>
 
 
-        {/* Meet  */}
-        <div className="  container mt-8 rounded-2xl bg-white w-full h-[819px] pt-40 text-black"></div>
+        {/* Meet */}
+        <div className="container mt-8 rounded-2xl bg-white w-full h-[819px] pt-20 text-black relative">
+          <div className="flex justify-start items-start h-full px-12 py-6 space-x-2">
+            <div className="text-left">
+              <p className="font-bricolage text-3xl font-normal mb-2 text-on-surface">
+                Fueled by 100% volunteer passion, Growing with 264 dedicated hearts.
+              </p>
+              <p className="font-bricolage text-3xl font-normal mb-8 text-on-surface">
+                Empowered by community support
+              </p>
+              <div className="flex justify-start items-start">
+                {/* Container for Image 1 and Content Box */}
+                <div className="relative flex">
+
+                  <div style={{ padding: '16px' }}>
+                    <img src={pic1} alt="Pic 1" />
+
+                    <div className="p-4 bg-[#F2F6D8] rounded-b-[30px] text-center">
+                      <p className="text-lg font-open-sans">NY Attorney General</p>
+                      <p className="text-xl font-bricolage">Letitia James</p>
+                    </div>
+                    <div style={{ padding: '36px' }}>
+                      <button
+                        style={{
+                          display: 'flex',
+                          alignItems: 'center',
+                          justifyContent: 'center',
+                          padding: '12px 24px',
+                          background: '#5F36D6',
+                          border: 'none',
+                          borderRadius: '30px',
+                          cursor: 'pointer',
+                        }}
+                      >
+                        <span className="text-white font-bricolage text-lg">More About Us</span>
+                      </button>
+                    </div>
+
+                  </div>
+
+                  <div style={{ padding: '16px' }}>
+                    <img src={pic2} alt="Pic 2" />
+
+                    <div className="p-4 bg-[#F2F6D8] rounded-b-[30px] text-center">
+                      <p className="text-lg font-open-sans">NYC Mayor</p>
+                      <p className="text-xl font-bricolage">Eric Adams</p>
+                    </div>
+
+                  </div>
+
+                </div>
+
+
+              </div>
+            </div>
+
+          </div>
+        </div>
+
 
         {/* Aniket */}
 
