214612058c7e72cf947838bd09ed4cc4d4daa5e6
Shreyas Shukla <shreyas@Shreyass-Air.localdomain>
2023-09-11 22:43:32 -0400

    fix: GH-Pages Deployment Router issue 🐛

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Home from "./component/Home";
-import logo from "./logo.png";
-import NavBar from "./component/Navbar";
-import Footer from "./component/Footer";
+import Home from './component/Home';
+import logo from './logo.png';
+import NavBar from './component/Navbar';
+import Footer from './component/Footer';
 
 function App() {
-  return (
-    // <div className="bg-black text-white justify-center w-screen h-screen">
-    //   <img src={logo} alt="logo" />
-    //   <p className="text-xl p-3">
-    //     Use <code>feature</code> branch to push your changes / make pull
-    //     request. Let's keep <code>main</code> branch clear for now.
-    //   </p>
-    //   <p className="text-xl p-3">
-    //     We will push to main on a feature basis, once we are done with homepage,
-    //     we will push it to main and later on use main branch to deploy our app.
-    //   </p>
-    //   <p className="text-xl p-3">
-    //     {" "}
-    //     Also if majority of developers are comfortable using Vite instead of
-    //     CRA, you can replace this boilerplate app with a Vite App before
-    //     starting to work on the project.
-    //   </p>
-    // </div>
-    <div>
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
-  );
+    return (
+        // <div className="bg-black text-white justify-center w-screen h-screen">
+        //   <img src={logo} alt="logo" />
+        //   <p className="text-xl p-3">
+        //     Use <code>feature</code> branch to push your changes / make pull
+        //     request. Let's keep <code>main</code> branch clear for now.
+        //   </p>
+        //   <p className="text-xl p-3">
+        //     We will push to main on a feature basis, once we are done with homepage,
+        //     we will push it to main and later on use main branch to deploy our app.
+        //   </p>
+        //   <p className="text-xl p-3">
+        //     {" "}
+        //     Also if majority of developers are comfortable using Vite instead of
+        //     CRA, you can replace this boilerplate app with a Vite App before
+        //     starting to work on the project.
+        //   </p>
+        // </div>
+        <div>
+            <Router>
+                <NavBar />
+                <Routes basename="https://logicb0mb.github.io/street_care_webapp/">
+                    <Route path="/" element={<Home />} />
+                </Routes>
+                <Footer />
+            </Router>
+        </div>
+    );
 }
 
 export default App;
