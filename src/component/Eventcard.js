8ae611f509f1eb744e642644f3aa457ccc1a64db
Nirav Patel <nnirav.patel7@gmail.com>
2023-09-13 17:54:11 -0400

    CSS update

--- src/component/Eventcard.js
+++ src/component/Eventcard.js
@@ -5,46 +5,42 @@ import wavingHand from "../images/waving_hand.png";
 
 const Eventcard = () => {
   return (
-    <div className="event-card bg-[#DEF6EB] rounded-lg p-8 lg:p-6 m-2">
-      <div class="flex items-center gap-1.5">
-        <img
-          class="w-9 h-9 md:w-9 md:h-9 rounded-full"
-          src={userImg}
-          alt="User Avatar"
-        />
+    <div className=" w-fit  bg-[#DEF6EB] rounded-lg p-8 lg:p-6 ">
+      <div class="flex items-center gap-1">
+        <img class="w-9 h-9 rounded-full" src={userImg} alt="User Avatar" />
         <div class="text-black text-xs font-normal">William Smith</div>
         <div class="bg-emerald-100 rounded-xl border border-stone-300 flex items-center px-2 py-1">
           <img class="w-4 h-4" src={verifiedImg} alt="Verified" />
-          <div class="text-zinc-700 text-xs  font-semibold leading-tight ">
+          <div class="text-zinc-700 text-sm  font-semibold leading-tight ">
             Certified Lead
           </div>
         </div>
       </div>
-      <div className="mt-2 md:mt-4">
-        <div className="text-neutral-800 text-sm md:text-lg font-medium leading-loose">
+      <div className="mt-2 lg:mt-4">
+        <div className="text-neutral-800 text-xl font-medium leading-normal">
           BK Fort Green Outreach
         </div>
-        <div className="text-zinc-700  md:text-sm font-semibold leading-normal">
+        <div className="text-zinc-700  text-[17px] font-semibold leading-normal">
           Sept 9, 2023 SAT 12:00pm
         </div>
         <div className="my-6 p-2 bg-white rounded-lg flex items-center gap-2.5">
           <img src={wavingHand} alt="Waving Hand" />
-          <div className="text-neutral-900 text-xs md:text-sm font-normal leading-snug">
+          <div className="text-neutral-900 text-sm font-normal leading-snug">
             Childcare specialist needed
           </div>
         </div>
         <div className="mt-2 md:mt-4 flex flex-col gap-1">
-          <div className="text-zinc-700 text-sm md:text-lg font-normal">
+          <div className="text-zinc-700 text-lg font-normal">
             200 Eastern Pkwy, Brooklyn, NY 11238
           </div>
-          <div className="text-zinc-700 text-sm md:text-lg font-normal">
+          <div className="text-zinc-700 text-lg font-normal">
             Open Spots: 8/20
           </div>
         </div>
       </div>
       <div className="mt-2 md:mt-4">
         <div className="pt-3 flex items-center gap-2.5">
-          <button className=" border-2 border-[#1F0A58] text-[#1F0A58] py-2 px-4 rounded-full text-xs md:text-sm">
+          <button className=" border-2 border-[#1F0A58] text-[#1F0A58] py-4 px-8 rounded-full text-sm ">
             Join
           </button>
         </div>
