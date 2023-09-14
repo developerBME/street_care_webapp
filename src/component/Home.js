4bdd2faa140e705ab080efe025e77fb5ac105830
Nirav Patel <nnirav.patel7@gmail.com>
2023-09-13 21:49:28 -0400

    small css update

--- src/component/Home.js
+++ src/component/Home.js
@@ -59,7 +59,86 @@ function Home() {
         </div>
 
         {/* Meet */}
+        <div className="container mt-8 rounded-2xl bg-white w-full h-[819px] pt-20 text-black relative">
+          <div className="flex justify-start items-start h-full px-12 py-6 space-x-2">
+            <div className="text-left">
+              <p className="font-bricolage text-3xl font-normal mb-2 text-on-surface">
+                Fueled by 100% volunteer passion, Growing with 264 dedicated
+                hearts.
+              </p>
+              <p className="font-bricolage text-3xl font-normal mb-8 text-on-surface">
+                Empowered by community support
+              </p>
+              <div className="flex justify-between items-start">
+                {/* Container for Image 1 and Content Box */}
+                <div className="flex flex-col items-center">
+                  <div style={{ padding: "16px" }}>
+                    <img src={pic1} alt="Pic 1" />
 
+                    <div className="p-4 bg-[#F2F6D8] rounded-b-[30px] text-center">
+                      <p className="text-lg font-open-sans">
+                        NY Attorney General
+                      </p>
+                      <p className="text-xl font-bricolage">Letitia James</p>
+                    </div>
+
+                    <div style={{ padding: "36px" }}>
+                      <button
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "center",
+                          padding: "12px 24px",
+                          background: "#5F36D6",
+                          border: "none",
+                          borderRadius: "30px",
+                          cursor: "pointer",
+                        }}
+                      >
+                        <span className="text-white font-bricolage text-lg">
+                          More About Us
+                        </span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+
+                {/* Container for Image 2 and Content Box */}
+                <div className="flex flex-col items-center">
+                  <div style={{ padding: "16px" }}>
+                    <img src={pic2} alt="Pic 2" />
+
+                    <div className="p-4 bg-[#F2F6D8] rounded-b-[30px] text-center">
+                      <p className="text-lg font-open-sans">NYC Mayor</p>
+                      <p className="text-xl font-bricolage">Eric Adams</p>
+                    </div>
+                  </div>
+                </div>
+
+                {/* New Quote Box */}
+                <div className="flex flex-col items-center mt-8">
+                  <div className="p-4 bg-[#F2F6D8] rounded-[30px] text-center max-w-[400px]">
+                    <div className="p-16 pt-6">
+                      {" "}
+                      {/* Added pt-4 */}
+                      <p className="text-lg font-bricolage mb-4">
+                        “A Saturday of service at Street Care’s Day of Outreach
+                        to Homeless Families and Individuals [on behalf of
+                        GivingTuesdayMilitary] gave this military spouse a big
+                        helping of much-needed perspective!”
+                      </p>
+                      <p className="mb-2 text-base font-lato">
+                        {" "}
+                        {/* Added space and author's name */}— Hope Guinn
+                        Bradley, Volunteer
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         {/* Aniket */}
 
         {/* <div className="  container mt-8 rounded-2xl bg-white w-full h-[750px] text-black">
