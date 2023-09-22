abba311bcba8c6e817c2489f92a1a7bfa2fce02a
vishnudut <vvdut99@gmail.com>
2023-09-21 21:41:37 -0400

    Removed repeating code

--- src/component/HomePage/Profile.js
+++ src/component/HomePage/Profile.js
@@ -129,40 +129,6 @@ function Profile() {
         </div>
 
         {/* Vishnu*/}
-        {/*
-        <div className="  w-[95%] md:w-[90%] lg:w-[80%] p-28 mx-2 lg:mx-40 mt-8 rounded-2xl bg-white text-black mb-10">
-          <div className="flex flex-col gap-14">
-            <div className="inline-flex space-x-16">
-              <div class="text-neutral-800 text-5xl font-medium font-bricolage leading-[52px]">
-                My Outreaches
-              </div>
-              <div className="inline-flex px-8 py-4  bg-violet-600 rounded-full gap-2">
-                <img src={icon} className="w-4 h-4 mt-1" />
-                <button className="rounded-full text-neutral-100 text-[14px]">
-                  Document my Outreach
-                </button>
-              </div>
-            </div>
-            <div className="inline-flex bg-[#F2F6D8] p-4 rounded-xl space-x-4">
-              <div className="text-neutral-800 text-[16px] font-medium font-bricolage leading-loose">
-                Congratulations! You have attended more than 1 outreach event.
-                Now you can host your own.
-              </div>
-              <div className="px-3 py-2 inline-flex bg-violet-600 rounded-full gap-2">
-                <img src={add} className="w-4 h-4" />
-                <button className="rounded-full text-neutral-100 text-[10px]">
-                  Create Outreach
-                </button>
-              </div>
-            </div>
-            <div className="grid grid-cols-4 gap-2 gap-y-16">
-              {cardData.map((cardData, index) => (
-                <UserProfileCard key={index} cardData={cardData} />
-              ))}
-            </div>
-          </div>{" "}
-        </div>
-              */}
       </div>
     </div>
   );
