b5150e776c28041f38dcbbf005ee310d7b9c735e
vishnudut <vvdut99@gmail.com>
2023-09-26 18:22:54 -0400

    Created the Outreach Form

--- src/component/Community/Community.js
+++ src/component/Community/Community.js
@@ -1,18 +1,35 @@
 import React from "react";
 
 import Landing from "../HomePage/Landing";
+import Form from "../Community/Form"
+import arrowBack from "../../images/arrowBack.png"
 
 function Community() {
   return (
+    
+    // <div className="bg-gradient-to-tr from-[#E4EEEA] from-10% via-[#E4EEEA] via-60% to-[#EAEEB5] to-90% bg-fixed">
+    //   <div className="relative flex flex-col items-center ">
+    //     <div className=" w-[95%] md:w-[90%] lg:w-[80%] mx-2 lg:mx-40 mt-32 rounded-2xl bg-white text-black ">
+    //       {" "}
+    //       {/* Remove this component and create a new one for your code  */}
+    //       <Form />
+    //     </div>
+    //   </div>
+    // </div>
+  
     <div className="bg-gradient-to-tr from-[#E4EEEA] from-10% via-[#E4EEEA] via-60% to-[#EAEEB5] to-90% bg-fixed">
+      <div className="pt-32 inline-flex pl-36 pb-4 ">
+            <img src={arrowBack}/>
+            <p className="font-semibold font-bricolage text-[22px]">Return to Profile</p>
+          </div>
       <div className="relative flex flex-col items-center ">
-        <div className=" w-[95%] md:w-[90%] lg:w-[80%] mx-2 lg:mx-40 mt-32 rounded-2xl bg-white text-black ">
-          {" "}
-          {/* Remove this component and create a new one for your code  */}
-          <Landing />
+        
+        <div className="rounded-2xl mx-2 mb-32 lg:mx-40 bg-[#F8F9F0] pt-[100px] pb-[100px] pr-[150px] pl-[150px]">
+          <Form/>
         </div>
       </div>
     </div>
+    
   );
 }
 
---
+++ src/component/Community/Form.js
@@ -0,0 +1,91 @@
+import React from "react";
+
+import Chip from "../Community/Chip"
+
+const chipList = ['Clothing','Education','Personal Care','Employment and Training','Food and water','Healthcare','Chinese','Spanish','Language(please specify)','Legal','Shelter','Transportation','LGBTQ Support','Technology Access','Social Integration','Pet Care']
+
+const Form = ()=>{
+    return(
+        <div>
+            <form className="space-y-6">
+                <div>
+                    <div className="text-5xl font-medium font-bricolage pb-16">Create Outreach Event</div>
+                    <div className="space-y-4">
+                        <div className="font-semibold font-bricolage text-[22px]">Event Information</div>
+                        <div className="space-y-1.5">
+                            <p className="font-semibold font-['Inter'] text-[15px]">Event Name*</p>
+                            <input type="text" className="px-4 w-full block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 " placeholder="Use Location by default for group meetup" />
+                        </div>
+                        <div className="space-y-1.5">
+                            <p className="font-semibold font-['Inter'] text-[15px]">Event Description</p>
+                            <input type="text" className="px-4 w-full block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 " placeholder="Details" />
+                        </div>
+                        <div className="space-y-1.5">
+                            <p className="font-semibold font-['Inter'] text-[15px]">Maximum capacity of participants allowed</p>
+                            <input type="text" className="w-full block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 " />
+                            <p className="font-normal font-['Inter'] text-xs">Please provide a numerical value.</p>
+                        </div>
+                        <div className="text-[22px] font-semibold font-bricolage">Meet up Details</div>
+                        <div className="space-y-1.5">
+                            <p className="font-semibold font-['Inter'] text-[15px]">Street*</p>
+                            <input type="text" className="px-4 w-full block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 "  placeholder="Street"/>
+                        </div>
+                        <div className="inline-flex grid grid-cols-2 space-x-4">
+                            <div className="space-y-1.5">
+                                <p className="font-semibold font-['Inter'] text-[15px]">State*</p>
+                                <input type="text" className="px-4 w-full block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 " placeholder="New York"/>
+                            </div>
+                            <div className="space-y-1.5">
+                                <p className="font-semibold font-['Inter'] text-[15px]">Zipcode*</p>
+                                <input type="text" className="px-4 w-full block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 " placeholder="11201"/>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="space-y-1.5">
+                                <p className="font-semibold font-['Inter'] text-[15px]">Date*</p>
+                                <input type="date" className="px-4 w-full block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 " />
+                                <p className="font-normal font-['Inter'] text-xs">Please follow the format mm/dd/yyyy</p>
+                            </div>
+                        </div>
+                        <div className="inline-flex space-x-4 grid grid-cols-2">
+                            <div className="space-y-1.5">
+                                <p className="font-semibold font-['Inter'] text-[15px]">Start Time*</p>
+                                <input type="time" className="px-4 w-full block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 "/>
+                            </div>
+                            <div className="space-y-1.5">
+                                <p className="font-semibold font-['Inter'] text-[15px]">End Time*</p>
+                                <input type="time" className="px-4 w-full block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 " />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="space-y-6">
+                    <div className="font-semibold font-bricolage text-[22px]">What kind of help they need?</div>
+                    <div className="space-y-1.5">  
+                        <p className="font-semibold font-['Inter'] text-[15px]">Is this Event related to any Help Request? </p>
+                        <select
+                            className="px-4 w-full block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring focus:ring-indigo-400"
+                            defaultValue=""
+                        >
+                            <option value="" disabled>Select Help Request</option>
+                            <option value="helpRequest1">Help Request 1</option>
+                            <option value="helpRequest2">Help Request 2</option>
+                        </select>
+                    </div>
+                    <div className="font-semibold font-bricolage text-[15px]">Select skills it would require to provide the help</div>
+                    <div className="w-[587px] space-y-2">
+                        {chipList.map((value, index) => (
+                            <Chip key={index} val={value} />
+                        ))}
+                    </div>
+                </div>
+                <div className="space-y-16 space-x-[15px]">
+                    <button className="px-8 py-4 border border-[#5F35D5] rounded-full text-violet-700">Cancel</button>
+                    <button className="px-8 py-4 border rounded-full bg-violet-700 text-[#F8F9F0]">Publish</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
