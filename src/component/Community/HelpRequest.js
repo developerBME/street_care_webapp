47aa391bd9d9b5cf715c49955032c3ec140cd213
aniket14thumar <aniket14.thumar@gmail.com>
2023-10-02 16:21:14 -0400

    Responsive HelpRequest component

--- src/component/Community/HelpRequest.js
+++ src/component/Community/HelpRequest.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import wavingHand from "../../images/waving_hand2.png";
 import help_announcement from "../../images/help_announcement.png";
 import help_pending from "../../images/help_pending.png";
 import help_received from "../../images/help_received.png";
 
 const HelpRequest = () => {
+  const [showLoadBtn, setShowLoadBtn] = useState(true);
   return (
     <div>
-      <div className="px-32 py-16 bg-gradient-to-br from-[#C0F4FF] to-[#DDD] rounded-t-2xl">
+      <div className="p-4 lg:px-28 lg:py-12 bg-gradient-to-br from-[#C0F4FF] to-[#DDD] rounded-t-2xl">
         <div className="flex gap-x-4 items-center">
           <div>
-            <img className="w-16 h-16" src={wavingHand} alt="..."></img>
+            <img
+              className="w-12 h-12 lg:w-16 lg:h-16"
+              src={wavingHand}
+              alt="..."
+            ></img>
           </div>
-          <div className="text-[#212121] text-4xl font-medium font-bricolage leading-[44px]">
+          <div className="text-[#212121] text-2xl lg:text-4xl font-medium font-bricolage lg:leading-[44px]">
             Help Requests
           </div>
           <button className="my-2 bg-[#6840E0] rounded-[100px] flex-col justify-center items-center gap-2 inline-flex text-neutral-100 text-md font-semibold font-inter leading-tight self-stretch px-6 py-2.5">
@@ -20,10 +25,10 @@ const HelpRequest = () => {
           </button>
         </div>
       </div>
-      <div className="px-32 pb-32 pt-16 flex flex-col bg-[#F7F7F7] gap-8 rounded-b-2xl">
+      <div className="p-4 lg:px-28 lg:py-12 flex flex-col bg-[#F7F7F7] gap-4 lg:gap-8 rounded-b-2xl">
         <div>
-          <div className="p-8 gap-16 bg-white border-y border-[#C8C8C8] justify-end">
-            <div className="flex gap-x-8">
+          <div className="p-4 lg:p-8 gap-16 bg-white border-y border-[#C8C8C8] justify-end">
+            <div className="flex gap-x-2 md:gap-x-4 lg:gap-x-8">
               <div className="w-fit h-fit flex-col justify-start items-start gap-[15px] inline-flex">
                 <div className="w-fit h-8 px-2 py-1 bg-[#FFECF2] rounded-lg justify-start items-start gap-2 inline-flex">
                   <div className="w-6 h-6 relative">
@@ -68,16 +73,16 @@ const HelpRequest = () => {
                   Posted 30 mins ago by user K
                 </div>
               </div>
-              <div className="w-[164px] h-fit flex-col justify-start items-end gap-4 inline-flex">
-                <button className="w-fit bg-[#E6DCFF] hover:bg-[#6840E0] text-[#181818] hover:text-white rounded-[100px] flex-col justify-start gap-2 flex px-6 py-2.5 text-center text-[11px] font-semibold font-inter leading-tight">
+              <div className="w-1/3 h-fit flex-col justify-start items-end gap-4 inline-flex">
+                <button className="w-fit bg-[#E6DCFF] hover:bg-[#6840E0] text-[#181818] hover:text-white rounded-[100px] flex-col justify-start gap-2 flex px-4 py-2 md:px-6 md:py-2.5 text-center text-[12px] font-semibold font-inter leading-tight">
                   I can help
                 </button>
               </div>
             </div>
           </div>
           {/*  */}
-          <div className="p-8 gap-8 bg-white border-y border-[#C8C8C8]justify-end">
-            <div className="flex gap-x-8 flex-start">
+          <div className="p-4 lg:p-8 gap-16 bg-white border-y border-[#C8C8C8]justify-end">
+            <div className="flex gap-x-2 md:gap-x-4 lg:gap-x-8">
               <div className="w-fit h-fit flex-col justify-start items-start gap-[15px] inline-flex">
                 <div className="w-fit h-8 px-2 py-1 bg-[#FEF9EF] rounded-lg justify-start items-start gap-2 inline-flex">
                   <div className="w-6 h-6 relative">
@@ -121,19 +126,19 @@ const HelpRequest = () => {
                   Posted 30 mins ago by user K
                 </div>
               </div>
-              <div className="flex-col justify-start items-end gap-2.5 inline-flex">
-                <button className="bg-[#E6DCFF] hover:bg-[#6840E0] text-[#181818] hover:text-white rounded-[100px] flex-col justify-center items-center gap-2 inline-flex text-[11px] font-semibold font-inter leading-tight self-stretch px-6 py-2.5 text-center">
+              <div className="w-1/3 h-fit flex-col justify-start items-end gap-4 inline-flex">
+                <button className="w-fit bg-[#E6DCFF] hover:bg-[#6840E0] text-[#181818] hover:text-white rounded-[100px] flex-col justify-start gap-2 flex px-4 py-2 md:px-6 md:py-2.5 text-center text-[12px] font-semibold font-inter leading-tight">
                   Mark as Help Recieved
                 </button>
-                <button className="bg-[#6840E0] hover:bg-[#E6DCFF] text-white hover:text-[#181818] rounded-[100px] flex-col justify-center items-center gap-2 inline-flex text-center text-[11px] font-semibold font-inter leading-tight self-stretch px-6 py-2.5">
+                <button className="w-fit bg-[#6840E0] hover:bg-[#E6DCFF] text-white hover:text-[#181818] rounded-[100px] flex-col justify-start gap-2 flex px-4 py-2 md:px-6 md:py-2.5 text-center text-[12px] font-semibold font-inter leading-tight">
                   Reopen Help Request
                 </button>
               </div>
             </div>
           </div>
           {/*  */}
-          <div className="p-8 gap-16 bg-white border-y border-[#C8C8C8] justify-end">
-            <div className="flex gap-x-8">
+          <div className="p-4 lg:p-8 gap-16 bg-white border-y border-[#C8C8C8] justify-end">
+            <div className="flex gap-x-2 md:gap-x-4 lg:gap-x-8">
               <div className="w-fit h-fit flex-col justify-start items-start gap-[15px] inline-flex">
                 <div className="w-fit h-8 px-2 py-1 bg-[#D4FFEC] rounded-lg justify-start items-start gap-2 inline-flex">
                   <div className="w-6 h-6 relative">
@@ -181,8 +186,8 @@ const HelpRequest = () => {
             </div>
           </div>
           {/*  */}
-          <div className="p-8 gap-16 bg-white border-y border-[#C8C8C8] justify-end">
-            <div className="flex gap-x-8">
+          <div className="p-4 lg:p-8 gap-16 bg-white border-y border-[#C8C8C8] justify-end">
+            <div className="flex gap-x-2 md:gap-x-4 lg:gap-x-8">
               <div className="w-fit h-fit flex-col justify-start items-start gap-[15px] inline-flex">
                 <div className="w-fit h-8 px-2 py-1 bg-[#D4FFEC] rounded-lg justify-start items-start gap-2 inline-flex">
                   <div className="w-6 h-6 relative">
@@ -230,8 +235,8 @@ const HelpRequest = () => {
             </div>
           </div>
           {/*  */}
-          <div className="p-8 gap-16 bg-white border-y border-[#C8C8C8] justify-end">
-            <div className="flex gap-x-8">
+          <div className="p-4 lg:p-8 gap-16 bg-white border-y border-[#C8C8C8] justify-end">
+            <div className="flex gap-x-2 md:gap-x-4 lg:gap-x-8">
               <div className="w-fit h-fit flex-col justify-start items-start gap-[15px] inline-flex">
                 <div className="w-fit h-8 px-2 py-1 bg-[#D4FFEC] rounded-lg justify-start items-start gap-2 inline-flex">
                   <div className="w-6 h-6 relative">
@@ -279,9 +284,32 @@ const HelpRequest = () => {
             </div>
           </div>
         </div>
-        <button className="w-fit rounded-[100px] border border-[#C8C8C8] flex-col justify-center items-center gap-2 flex text-center text-[#1F0A58] hover:bg-[#1F0A58] hover:text-white text-[11px] font-semibold font-inter leading-tight self-stretch px-6 py-2.5">
+
+        {showLoadBtn ? (
+          <button
+            className="w-fit rounded-[100px] border border-[#C8C8C8] flex-col justify-center items-center gap-2 flex text-center text-[#1F0A58] hover:bg-[#1F0A58] hover:text-white text-[11px] font-semibold font-inter leading-tight self-stretch px-6 py-2.5"
+            onClick={() => {
+              setShowLoadBtn(false);
+            }}
+          >
+            Load 5 More
+          </button>
+        ) : (
+          <button
+            className="w-fit rounded-[100px] border border-[#C8C8C8] flex-col justify-center items-center gap-2 flex text-center text-[#1F0A58] hover:bg-[#1F0A58] hover:text-white text-[11px] font-semibold font-inter leading-tight self-stretch px-6 py-2.5"
+            onClick={() => {
+              setShowLoadBtn(true);
+            }}
+          >
+            Hide
+          </button>
+        )}
+        {/* <button
+          className="w-fit rounded-[100px] border border-[#C8C8C8] flex-col justify-center items-center gap-2 flex text-center text-[#1F0A58] hover:bg-[#1F0A58] hover:text-white text-[11px] font-semibold font-inter leading-tight self-stretch px-6 py-2.5"
+          onClick={() => {}}
+        >
           Load 5 More
-        </button>
+        </button> */}
       </div>
     </div>
   );
