db17bfe6797a03a62521277a311cb0deefd2db54
chiknes <shailesh.vit@gmail.com>
2023-10-24 17:57:51 -0400

    MErged conflict resolved

--- src/component/HomePage/BMEofficialCard.js
+++ src/component/HomePage/BMEofficialCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import imgBME from "../../images/imgBME.png";
 import CustomButton from "../Buttons/CustomButton";
 import { handleRsvp } from "../EventCardService";
