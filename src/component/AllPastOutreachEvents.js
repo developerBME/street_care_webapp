a9091cab1d07cce36619244701855825fb7c1190
Vedant Chokshi <vedchokshi1597@gmail.com>
2023-11-17 22:35:13 -0500

    SearchBar Update

--- src/component/AllPastOutreachEvents.js
+++ src/component/AllPastOutreachEvents.js
@@ -76,7 +76,7 @@ const AllPastOutreachEvents = () => {
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 pointer-events-none absolute top-6 transform -translate-y-1/2 left-3">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                           </svg>
-                          <input type="email" name="email" id="email" placeholder="Search" ref={searchRef}
+                          <input type="email" name="email" id="email" placeholder="Search Outreach Events" ref={searchRef}
                             onChange={searchChange} class="form-input border py-3 px-4 bg-white placeholder-gray-400 text-gray-500 appearance-none w-full block pl-12 focus:outline-none rounded-2xl"/>
                       </label>
                 </div>
