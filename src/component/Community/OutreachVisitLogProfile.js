f2a857c9c5ab9230a574825609d7d0888cf3e8ca
Vedant Chokshi <vedchokshi1597@gmail.com>
2023-12-21 08:09:13 -0500

    Load more in ProfileVisitlog Cards Updated

--- src/component/Community/OutreachVisitLogProfile.js
+++ src/component/Community/OutreachVisitLogProfile.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import OutreachVisitLogCard from "./OutreachVisitLogCard";
-import { fetchEvents } from "../EventCardService";
+import { fetchEvents, formatDate } from "../EventCardService";
 import { fetchVisitLogs } from "../VisitLogCardService";
 import EventCardSkeleton from "../Skeletons/EventCardSkeleton";
    
@@ -10,51 +10,6 @@ const OutreachVisitLogProfile = () => {
   const loadMore = () => {
     setVisibleItems((prev) => prev + 3);
   };
-
-  const cardData = [
-    {
-      userName: "William Smith",
-      title: "BK Fort Green Outreach",
-      eventDate: "Sept 9, 2023 SAT 5:00pm",
-      location: {
-        street: "200 Eastern Pkwy",
-        city: "Brooklyn",
-        state: "NY",
-        zipcode: "11238",
-      },
-      helpType: "Childcare Specialist needed",
-      totalSlots: 20,
-      interests: 5,
-    },
-    {
-      userName: "William Smith",
-      title: "BK Fort Green Outreach",
-      eventDate: "Sept 9, 2023 SAT 5:00pm",
-      location: {
-        street: "200 Eastern Pkwy",
-        city: "Brooklyn",
-        state: "NY",
-        zipcode: "11238",
-      },
-      helpType: "Childcare Specialist needed",
-      totalSlots: 20,
-      interests: 5,
-    },
-    {
-      userName: "William Smith",
-      title: "BK Fort Green Outreach",
-      eventDate: "Sept 9, 2023 SAT 5:00pm",
-      location: {
-        street: "200 Eastern Pkwy",
-        city: "Brooklyn",
-        state: "NY",
-        zipcode: "11238",
-      },
-      helpType: "Childcare Specialist needed",
-      totalSlots: 20,
-      interests: 5,
-    },
-  ];
   
   const [events, setEvents] = useState([]);
   const [visitLogs, setVisitLogs] = useState([]);
@@ -98,6 +53,8 @@ const OutreachVisitLogProfile = () => {
   }, [eventsDisplay]);
 
 
+
+
   return (
     <div>
         <div className="flex items-center justify-between">
@@ -118,16 +75,26 @@ const OutreachVisitLogProfile = () => {
                 <EventCardSkeleton />
             </div>
             ):(<div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2">
-            {(visitLogs.slice(0,3).map((visitLogData) => (
+            {/* {(visitLogs.slice(0,3).map((visitLogData) => (
             <OutreachVisitLogCard visitLogCardData={visitLogData}/>
-                )))}
+                )))} */}
+
+            {visitLogs.length > 0 &&
+                visitLogs.slice(0, visibleItems).map((visitLogData, index) => (
+                  <OutreachVisitLogCard
+                    
+                    key={index}
+                    visitLogCardData={visitLogData}
+                  />
+                ))}
 
 
-        </div>)}
+        </div>
+        )}
             
-            {visibleItems < cardData.length && (
+            {visibleItems < visitLogs.length && (
             <button
-                className="w-full px-6 py-2.5 rounded-full text-sm font-medium text-violet-950 font-['DM Sans'] border border-stone-300"
+                className="w-fit rounded-[100px] border border-[#C8C8C8] flex-col justify-center items-center gap-2 flex text-center text-[#1F0A58] hover:bg-[#1F0A58] hover:text-white text-[13px] font-medium font-dmsans leading-tight self-stretch px-6 py-2.5 mt-7"
                 onClick={loadMore}
             >
                 Load More
