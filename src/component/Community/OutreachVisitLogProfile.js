223c86c149f91f598ff9e14612781fc376357981
Vedant Chokshi <vedchokshi1597@gmail.com>
2023-12-25 17:53:10 -0500

    NoOUtreachDoc Error Page Added !

--- src/component/Community/OutreachVisitLogProfile.js
+++ src/component/Community/OutreachVisitLogProfile.js
@@ -3,6 +3,7 @@ import OutreachVisitLogCard from "./OutreachVisitLogCard";
 import { fetchEvents, formatDate } from "../EventCardService";
 import { fetchVisitLogs } from "../VisitLogCardService";
 import EventCardSkeleton from "../Skeletons/EventCardSkeleton";
+import NoOutreachDoc from "./NoOutreachDoc";
    
 const OutreachVisitLogProfile = () => {  
 
@@ -16,26 +17,32 @@ const OutreachVisitLogProfile = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [eventsDisplay, setEventsDisplay] = useState([]);
   const [, setStates] = useState([]);
+  const [error, setError] = useState(null);
   
   useEffect(() => {
     const fetchData = async () => {
-      const eventsData = await fetchEvents();
-      const visitLogsData = await fetchVisitLogs();
-      setVisitLogs(visitLogsData)
-      // Filter events to get only past events
-      const upcomingEvents = eventsData
-        .filter((event) => {
-          const eventDate = new Date(event.eventDate.seconds * 1000);
-          return eventDate >= new Date(); // Check if the event date is before the current date
-        })
-      // Sort events in place based on their date
-      upcomingEvents.sort((a, b) => a.eventDate - b.eventDate);
+      try {
+        const eventsData = await fetchEvents();
+        const visitLogsData = await fetchVisitLogs();
+        setVisitLogs(visitLogsData)
+        // Filter events to get only past events
+        const upcomingEvents = eventsData
+          .filter((event) => {
+            const eventDate = new Date(event.eventDate.seconds * 1000);
+            return eventDate >= new Date(); // Check if the event date is before the current date
+          })
+        // Sort events in place based on their date
+        upcomingEvents.sort((a, b) => a.eventDate - b.eventDate);
 
-      setEvents(upcomingEvents);
-      // Extract states and remove duplicates
-      const extractedStates = [...new Set(upcomingEvents.map(event => event.location.state))];
-      setStates(extractedStates);
+        setEvents(upcomingEvents);
+        // Extract states and remove duplicates
+        const extractedStates = [...new Set(upcomingEvents.map(event => event.location.state))];
+        setStates(extractedStates);
       
+    } catch (error) {
+      setError(error);
+      setIsLoading(false);
+    }
     };
 
     fetchData();
@@ -52,57 +59,48 @@ const OutreachVisitLogProfile = () => {
     }
   }, [eventsDisplay]);
 
-
-
-
   return (
     <div>
-        <div className="flex items-center justify-between">
-            <div className="md:inline-flex items-center text-center mb-7 space-y-2 md:space-y-0">
-                <p className="font-medium text-xl lg:text-2xl text-[#212121] font-bricolage">
-                Outreach Visit Log
-                </p>
-                <button className="text-sm font-medium font-['DM Sans'] leading-tight lg:text-[12px] text-white bg-[#6840E0] px-6 py-2.5 lg:px-6 lg:py-2.5 rounded-full sm:hidden">
-                Create an Outreach
-                </button>
-            </div>
-
-            </div>
-            {isLoading?(
-            <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2">
-                <EventCardSkeleton />
-                <EventCardSkeleton />
-                <EventCardSkeleton />
-            </div>
-            ):(<div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2">
-            {/* {(visitLogs.slice(0,3).map((visitLogData) => (
-            <OutreachVisitLogCard visitLogCardData={visitLogData}/>
-                )))} */}
-
-            {visitLogs.length > 0 &&
-                visitLogs.slice(0, visibleItems).map((visitLogData, index) => (
-                  <OutreachVisitLogCard
-                    
-                    key={index}
-                    visitLogCardData={visitLogData}
-                  />
-                ))}
-
-
+      <div className="flex items-center justify-between">
+        <div className="md:inline-flex items-center text-center mb-7 space-y-2 md:space-y-0">
+          <p className="font-medium text-xl lg:text-2xl text-[#212121] font-bricolage">
+            Outreach Visit Log
+          </p>
         </div>
-        )}
-            
-            {visibleItems < visitLogs.length && (
-            <button
-                className="w-fit rounded-[100px] border border-[#C8C8C8] flex-col justify-center items-center gap-2 flex text-center text-[#1F0A58] hover:bg-[#1F0A58] hover:text-white text-[13px] font-medium font-dmsans leading-tight self-stretch px-6 py-2.5 mt-7"
-                onClick={loadMore}
-            >
-                Load More
-            </button>
-            )}
-    </div>
+      </div>
 
-   );
+      {isLoading ? (
+        <div className=" sm:block sm:overflow-x-auto overflow-y-hidden flex justify-between items-center w-full">
+          <EventCardSkeleton />
+          <EventCardSkeleton />
+          <EventCardSkeleton />
+        </div>
+      ) : (
+        <>
+          {visitLogs.length > 0 && (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-y-2 mb-6">
+              {visitLogs.slice(0, visibleItems).map((visitLogData, index) => (
+                <div key={index} className="p-2">
+                  <OutreachVisitLogCard visitLogCardData={visitLogData} />
+                </div>
+              ))}
+            </div>
+          )}
+
+          {visibleItems < visitLogs.length && (
+            <button
+              className="w-fit rounded-[100px] border border-[#C8C8C8] flex-col justify-center items-center gap-2 flex text-center text-[#1F0A58] hover:bg-[#1F0A58] hover:text-white text-[13px] font-medium font-dmsans leading-tight self-stretch px-6 py-2.5 mt-7"
+              onClick={loadMore}
+            >
+              Load More
+            </button>
+          )}
+
+          {visitLogs.length === 0 && <NoOutreachDoc />}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default OutreachVisitLogProfile;
--- src/component/UserProfile/Profile.js
+++ src/component/UserProfile/Profile.js
@@ -1,108 +1,33 @@
 import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import UserProfileCard from "./UserProfileCard";
+import { useNavigate } from "react-router-dom";
 import icon from "../../images/icon.png";
 import add from "../../images/add.png";
 import UserInfo from "./UserInfo";
-import axc from "./CommOutForm";
 import EventCardSkeleton from "../Skeletons/EventCardSkeleton";
-import arrowDropDown from "../../images/arrowDropDown.png";
-import arrowRight from "../../images/arrowRight.png";
 import OutreachEventCard from "../Community/OutreachEventCard";
-import OutreachVisitLogCard from "../Community/OutreachVisitLogCard";
 import { formatDate, fetchUserEvents } from "../EventCardService";
 import { auth } from "../firebase";
-
 import CustomButton from "../Buttons/CustomButton";
-
 import { fetchEvents } from "../EventCardService";
 import { fetchPersonalVisitLogs } from "../VisitLogCardService";
 import OutreachVisitLogProfile from "../Community/OutreachVisitLogProfile";
+import NoOutreachDoc from "../Community/NoOutreachDoc";
 
 
 function Profile() {
-  const cardData = [
-    {
-      title: "BME Official Event",
-      eventName: "Brooklyn Fort Green Outreach",
-      eventDate: "Nov 9, 2023 FRI 5:00pm",
-      buttonText: "Upcoming...",
-      cardColor: "#F1EEFE",
-      upcomingEvent: 1,
-    },
-    {
-      title: "Group",
-      eventName: "Jersey City Hoboken Outreach",
-      eventDate: "Oct 16, 2023 SAT 5:00pm",
-      buttonText: "Add Visit Log",
-      cardColor: "#F1EEFE",
-      upcomingEvent: 0,
-    },
-    {
-      title: "Group",
-      eventName: "Brooklyn Williamsburg Outreach",
-      eventDate: "Oct 4, 2023 SAT 5:00pm",
-      buttonText: "Add Visit Log",
-      cardColor: "#F1EEFE",
-      upcomingEvent: 0,
-    },
-    {
-      title: "Personal",
-      eventName: "Manhatten Harlem Outreach",
-      eventDate: "Apr 12, 2023 SAT 5:00pm",
-      buttonText: "Edit Details",
-      cardColor: "#DEF6EB",
-      upcomingEvent: 0,
-    },
-    {
-      title: "BME Official Event",
-      eventName: "BK Fort Green Outreach",
-      eventDate: "Sept 12, 2023 SAT 12:00pm",
-      buttonText: "Edit Details",
-      cardColor: "#F1EEFE",
-      upcomingEvent: 1,
-    },
-    {
-      title: "Personal",
-      eventName: "Manhatten Harlem Outreach",
-      eventDate: "Apr 12, 2023 SAT 5:00pm",
-      buttonText: "Edit Details",
-      cardColor: "#DEF6EB",
-      upcomingEvent: 0,
-    },
-    {
-      title: "Group",
-      eventName: "Brookolyn Museum Outreach",
-      eventDate: "12/12/2023 SAT 5:00pm",
-      buttonText: "Add Visit Log",
-      cardColor: "#F1EEFE",
-      upcomingEvent: 0,
-    },
-    {
-      title: "Personal",
-      eventName: "Manhatten Harlem Outreach",
-      eventDate: "Apr 12, 2023 SAT 5:00pm",
-      buttonText: "Add Visit Log",
-      cardColor: "#F1EEFE",
-      upcomingEvent: 0,
-    },
-  ];
 
   const [events, setEvents] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [eventsDisplay, setEventsDisplay] = useState([]);
   const navigate = useNavigate();
  
-  const [filteredEvents, setFilteredEvents] = useState([]);
-  const [states, setStates] = useState([]);
-  const [selectedState, setSelectedState] = useState("");
-  const [dropdownVisible, setDropdownVisible] = useState(false);
-  const [visitLogs, setVisitLogs] = useState([]);
-  const [visibleItems, setVisibleItems] = useState(3);
+ 
+  const [,setStates] = useState([]);
+  const [] = useState("");
+  const [] = useState(false);
+  const [, setVisitLogs] = useState([]);
+ 
 
-  const loadMore = () => {
-    setVisibleItems((prev) => prev + 3);
-  };
   const [activeTab, setActiveTab] = useState("myoutreach");
 
 
@@ -181,19 +106,7 @@ function Profile() {
     }
   }, [eventsDisplay]);
 
-  const handleStateSelection = (state) => {
-    setSelectedState(state);
-    const filtered = events.filter(event => event.location.state === state);
-    setFilteredEvents(filtered);
-    setDropdownVisible(false);
-  };
 
-  const upcomingEvents = events
-    .filter((event) => {
-      const eventDate = new Date(event.eventDate.seconds * 1000);
-      return eventDate >= new Date(); // Check if the event date is before the current date
-    })
-    .slice(0, 3);
 
   return (
     <div className="bg-gradient-to-tr from-[#E4EEEA] from-10% via-[#E4EEEA] via-60% to-[#EAEEB5] to-90% bg-fixed">
@@ -247,16 +160,6 @@ function Profile() {
                   navigate("/profile/select-outreach");
                 }}
               />
-              {/* <div className="inline-flex bg-violet-600 rounded-full gap-2 items-center p-2 lg:p-4 mr-44 mt-2 lg:mt-0">
-                <img src={icon} className="w-4 h-4 mt-1" />
-
-                <Link
-                  to={"/profile/select-outreach"}
-                  className="w-fit rounded-full text-neutral-100 text-[10px] lg:text-[14px] "
-                >
-                  Document my Outreach
-                </Link>
-              </div> */}
             </div>
             <div className="">
               <div className="w-full flex flex-col sm:flex-row bg-[#F2F6D8] p-4 rounded-xl gap-4  ">
@@ -274,39 +177,9 @@ function Profile() {
                     }}
                   />
                 </div>
-                {/* <div className="px-3 py-2 inline-flex bg-violet-600 rounded-full gap-2">
-                  <img src={add} className="w-4 h-4" />
-                  <Link className="rounded-full text-neutral-100 text-[10px]">
-                    Create Outreach
-                  </Link>
-                </div> */}
               </div>
             </div>
-            <div className="sm:hidden overflow-x-auto overflow-y-hidden">
-            {isLoading ? (
-              <div className="flex justify-between items-center w-full h-fit gap-2">
-                <EventCardSkeleton />
-                <EventCardSkeleton />
-                <EventCardSkeleton />
-              </div>
-            ):(
-              <div className="flex space-x-3 w-fit lg:p-4">
-                {events.map((eventData) => (
-                  <OutreachEventCard
-                    key={eventData.id}
-                    cardData={{
-                      ...eventData,
-                      eventDate: formatDate(
-                        new Date(eventData.eventDate.seconds * 1000)
-                      ),
-                    }}
-                    isProfilePage={true}
-                    refresh={fetchData}
-                  />
-                ))}
-              </div>
-            )}
-            </div>
+            
             <div className="hidden sm:block sm:overflow-x-auto overflow-y-hidden">
             {isLoading? (
               <div className="flex justify-between items-center w-full h-fit gap-2">
@@ -314,7 +187,9 @@ function Profile() {
                 <EventCardSkeleton />
                 <EventCardSkeleton />
               </div>
-            ):(
+            ) : events.length === 0 ? (
+              <NoOutreachDoc />
+            ) : (
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2 gap-y-16 mb-6">
                 {events.map((eventData) => (
                   <OutreachEventCard
@@ -341,15 +216,7 @@ function Profile() {
         <OutreachVisitLogProfile/>
 </div>
 
-
-
-
-
-{/* Vishnu
-
-
-
-*/}
+{/* Vishnu*/}
 
 </div>
 )
