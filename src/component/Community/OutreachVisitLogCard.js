de3b3f99a5b8ab6faf866f1237b689ce519565f8
Vedant Chokshi <vedchokshi1597@gmail.com>
2024-01-29 18:02:58 -0500

    Visit Log Update

--- src/component/Community/OutreachVisitLogCard.js
+++ src/component/Community/OutreachVisitLogCard.js
@@ -1,6 +1,8 @@
 import React from "react"
 import profilePic from "../../images/avatar.jpg"
 import wavingHand from "../../images/waving_hand.png"
+import location from "../../images/location_on.svg"
+import calender from "../../images/calendar_month.svg"
 import CustomButton from "../Buttons/CustomButton"
 import defaultImage from "../../images/default_avatar.svg";
 import { useNavigate } from "react-router-dom";
@@ -22,40 +24,70 @@ const OutreachVisitLogCard = ({visitLogCardData, showProfileInfo})=>{
             <div className="flex-1 px-6 py-2">
                 <div className="space-y-4">
                 {showProfileInfo && (
-                        <div class="text-violet-900 text-sm font-medium font-['DM Sans'] leading-tight">{visitLogCardData.date || ""} · {visitLogCardData?.location?.street || ""} {visitLogCardData?.city || ""} {visitLogCardData?.state || ""} {visitLogCardData?.location?.zipcode || ""}</div>
+                        <div class="text-violet-900 text-sm font-medium font-bricolage leading-tight">{visitLogCardData.date || ""} · {visitLogCardData?.location?.street || ""} {visitLogCardData?.city || ""} {visitLogCardData?.state || ""} {visitLogCardData?.location?.zipcode || ""}</div>
                 )}
 
                 {!showProfileInfo && (
                     <div>
-                        <div className="text-violet-900 py-1 text-sm font-medium font-['DM Sans'] leading-tight">{visitLogCardData.date || ""} {visitLogCardData.time}</div>
-                        <div className="font-normal font-inter py-1 text-[#444746] text-[13px]">{visitLogCardData?.location?.street || ""} {visitLogCardData?.city || ""} {visitLogCardData?.state || ""} {visitLogCardData?.location?.zipcode || ""}</div>
-                        <div className="text-zinc-700 py-1 text-[12px] font-normal font-['DM Sans'] leading-snug">People Helped : {visitLogCardData.numberPeopleHelped || ""} Items : {visitLogCardData.itemQty || ""} Rating : {visitLogCardData.rating || ""} </div>
+                        <div className="lg:flex lg:items-center mt-4 lg:mt-0">
+                            <div className="text-violet-900 lg:mr-10 text-sm font-medium font-bricolage leading-tight">{<img src={calender} />}{visitLogCardData.date || ""} · {visitLogCardData.time}</div>
+                            <div className="text-violet-900 lg:text-right py-1 text-sm font-medium font-bricolage leading-tight"> {<img src={location} />}{visitLogCardData?.city || ""}, {visitLogCardData?.state || ""} </div>
+                        </div>
+                        <div class="text-zinc-700 text-[10px] font-normal font-bricolage leading-snug">{visitLogCardData.description || ""}</div>
+                        <div className="flex justify-between">
+                            <div className="text-neutral-900 text-[12px] py-1 font-bold font-bricolage leading-tight text-left">People Helped</div>
+                            <div className="text-neutral-900 text-sm font-bold font-bricolage leading-tight text-right">{visitLogCardData.numberPeopleHelped || ""} </div>
+                        </div>
+                        <div className="flex justify-between">
+                            <div className="text-neutral-900 text-[12px] py-1 font-bold font-bricolage leading-tight text-left">Items Donated</div>  
+                            <div className="text-neutral-900 text-sm font-bold font-bricolage leading-tight text-right">{visitLogCardData.itemQty || ""} </div>
+                        </div>
+                        <div className=""> 
+                                 <div className="inline-flex items-center gap-2 flex-wrap">
+                                     {visitLogCardData?.whatGiven.map((item, index) => (
+                                     <div className="py-1 px-3 border border-[#C8C8C8] w-fit rounded-xl text-[10px] text-[#444746]">{item}</div>
+                                     ))}
+                                 </div>
+                        </div>
                     </div>
                 )}
-                    
-                    <div class="text-zinc-700 text-[10px] font-normal font-['DM Sans'] leading-snug">{visitLogCardData.description || ""}</div>
-                      <div className=""> 
-                        <div className="inline-flex items-center gap-2 flex-wrap">
-                            {visitLogCardData?.whatGiven.map((item, index) => (
-                            <div className="py-1 px-3 border border-[#C8C8C8] w-fit rounded-xl text-[10px] text-[#444746]">{item}</div>
-                            ))}
-                        </div>
-                      </div>
-                    <div className="w-full inline-flex items-center bg-white px-4 py-1 space-x-2.5 rounded-2xl">
-                        <img src={wavingHand} />
-                        <div className="font-normal font-['Inter'] text-[10px] text-[#181818]">
-                        {visitLogCardData.helpType || ""}
-                        </div>
-                    </div>
+                    {showProfileInfo && (
+                            <div>
+                               <div class="text-zinc-700 text-[10px] font-normal font-bricolage leading-snug">{visitLogCardData.description || ""}</div>
+                               <div className=""> 
+                                 <div className="inline-flex items-center gap-2 flex-wrap">
+                                     {visitLogCardData?.whatGiven.map((item, index) => (
+                                     <div className="py-1 px-3 border border-[#C8C8C8] w-fit rounded-xl text-[10px] text-[#444746]">{item}</div>
+                                     ))}
+                                 </div>
+                               </div>
+                            
+                        
+                                <div className="w-full inline-flex items-center bg-white px-4 py-1 space-x-2.5 rounded-2xl">
+                                    <img src={wavingHand} />
+                                    
+                                    <div className="font-normal font-['Inter'] text-[10px] text-[#181818]">
+                                    {visitLogCardData.helpType || ""}
+                                    </div>
+                                </div>
+                            </div>
+                     )
+                    }
                 </div>
             </div>
             <div className="pt-2 px-6 pb-6 space-x-2">
                 <CustomButton label="View Details" name="buttonlightsmall" onClick={() => {navigate(`/VisitLogDetails/${visitLogCardData.id}`);
                   }} />
-                <CustomButton label="Share" name="buttonsmallborder2"/>
+                  {showProfileInfo && (
+                        <CustomButton label="Share" name="buttonsmallborder2"/>)}
             </div>
         </div>
     )
 }
 
 export default OutreachVisitLogCard;
+
+
+
+
+
