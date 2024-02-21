5dd4838722f323a12e092ac5e7d79b579748da64
tejas-vivek <tejas.helwatkar@gmail.com>
2024-02-21 17:57:32 -0500

    Increased margin top of password div, increased font size of forgot password title, updated padding x and y on the text content

--- src/component/UserProfile/ForgotPassword.js
+++ src/component/UserProfile/ForgotPassword.js
@@ -49,9 +49,9 @@ const ForgotPassword = () => {
   return (
     <div className="bg-gradient-to-tr from-[#E4EEEA] from-10% via-[#E4EEEA] via-60% to-[#EAEEB5] to-90% bg-fixed">
       <div className="relative flex flex-col items-center ">
-        <div className="w-[95%] md:w-[90%] lg:w-[75%] mx-2 mt-34 mb-20 rounded-2xl bg-white text-black ">
+        <div className="w-[95%] md:w-[90%] lg:w-[75%] mx-2 mt-36 mb-20 rounded-2xl bg-white text-black ">
           
-          <div className="justify-center items-center inline-flex p-8 lg:py-20 lg:px-32 h-full w-full rounded-2xl bg-[#F7F7F7]">
+          <div className="justify-center items-center inline-flex lg:py-24 lg:px-32 h-full w-full rounded-2xl bg-[#F7F7F7]">
             {showCheckEmailBlock ? (
                 /* Check your email - Block */
                 <div className="flex-col justify-start items-start gap-8 inline-flex">
@@ -81,8 +81,8 @@ const ForgotPassword = () => {
             ) : (
                 /* Forgot Password - Block */
                 
-                <div className="lg:w-[63%] flex-col justify-start items-start gap-4 flex">
-                    <div className="text-neutral-800 text-5xl font-bricolage leading-[64px] font-medium">Forgot your password?</div>
+                <div className="lg:w-[86%] flex-col justify-start items-start gap-8 flex">
+                    <div className="text-neutral-800 text-6xl font-bricolage leading-[64px] font-medium">Forgot your password?</div>
                     <div className="text-black text-base font-normal font-['DM Sans'] leading-normal tracking-wide">We’ve got you covered. We’ll email you instructions to reset your password. Kindly provide us with the email associated with your profile.</div>
                     <div className="w-full md:w-[95%] flex-col justify-start items-start gap-2 md:gap-4 flex">
                         <div className=" w-full md:w-[95%] flex-col justify-start items-start gap-1.5 flex">
