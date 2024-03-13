75e5deb6e5fe3a84a073c94bfde8563fec8e5152
tejas-vivek <tejas.helwatkar@gmail.com>
2024-03-13 17:31:49 -0400

    Completed integrating Places API in sample form

--- src/component/Sample_form.js
+++ src/component/Sample_form.js
@@ -1,27 +1,83 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+export const GOOGLE_PLACES_API_KEY = "AIzaSyBpaLVj2EjhjCeHbTUXfcBhBoaQLVathvE";
+
+let autoComplete;
+
+const loadScript = (url, callback) => {
+  let script = document.createElement("script");
+  script.type = "text/javascript";
+
+  if (script.readyState) {
+    script.onreadystatechange = function () {
+      if (script.readyState === "loaded" || script.readyState === "complete") {
+      }
+    };
+  } else {
+    script.onload = () => callback();
+  }
+
+  script.src = url;
+  document.getElementsByTagName("head")[0].appendChild(script);
+};
+
 const Sample_form = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const [query, setQuery] = useState("");
-  const autoCompleteRef = useRef();
+  const autoCompleteRef = useRef(null);
+
+  const handleScriptLoad = (updateQuery, autoCompleteRef) => {
+    autoComplete = new window.google.maps.places.Autocomplete(
+      autoCompleteRef.current,
+      {}
+    );
+
+    autoComplete.addListener("place_changed", () => {
+      handlePlaceSelect(updateQuery);
+    });
+  };
+
+  const handlePlaceSelect = async (updateQuery) => {
+    const addressObject = await autoComplete.getPlace();
+
+    const query = addressObject.formatted_address;
+    updateQuery(query);
+    console.log({ query });
+
+    const latLng = {
+      lat: addressObject?.geometry?.location?.lat(),
+      lng: addressObject?.geometry?.location?.lng(),
+    };
+
+    console.log({ latLng });
+  };
+
+  useEffect(() => {
+    loadScript(
+      `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_PLACES_API_KEY}&libraries=places`,
+      () => handleScriptLoad(setQuery, autoCompleteRef)
+    );
+  }, []);
 
   return (
+    
     <div className="relative flex flex-col items-center ">
+      
       <div className="lg:w-[887px] md:w-[90%] mx-2 lg:mx-40 mt-32 mb-32 md:mb-[55px] rounded-3xl bg-white text-black ">
         <div className="items-center justify-center px-12 py-8 lg:px-32 lg:py-24 h-full w-full rounded-3xl bg-[#F8F9F0] grid grid-cols-1">
-          <div className="w-fit h-fit flex-col justify-start items-start gap-16 inline-flex">
+          <div className="w-full h-fit flex-col justify-start items-start gap-16 inline-flex">
             <div className="w-fit text-[#212121] text-[45px] font-medium font-inter leading-[52px]">
               Sample Form
             </div>
-            <div>
+            <div className="w-full">
               <div className="flex flex-col">
                 <label>Enter Address</label>
                 <input
                   ref={autoCompleteRef}
-                  className="my-5 w-full h-10"
+                  className="my-5 w-full h-10 pl-3"
                   onChange={(event) => setQuery(event.target.value)}
-                  placeholder="Search a place"
+                  placeholder="Search for a place"
                   value={query}
                 ></input>
               </div>
