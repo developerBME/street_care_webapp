14dfba70c4bd9d91a237f488b9e9f6036128493f
aishwarya-katkar <katkar.a@northeastern.edu>
2024-03-17 17:07:26 -0400

    Update Email Changes

--- src/component/UserProfile/UpdateEmail.js
+++ src/component/UserProfile/UpdateEmail.js
@@ -1,26 +1,51 @@
 import { auth, db } from "../firebase";
+import { getAuth, updateEmail } from "firebase/auth";
+import {
+  doc,
+  getDoc,
+  updateDoc
+} from "firebase/firestore";
 
-async function updateEmailId(emailId, newEmailId) {
+
+
+export async function updateEmailId(newEmailId) {
   try {
     console.log(auth);
-    const user = auth.currentUser;
-    console.log(user);
+    const user = auth?.currentUser;
+    
+    console.log(newEmailId);
+    console.log(user?.email);
 
-    await user.updateEmail(newEmailId);
-
-    const uid = user.uid;
+    updateEmail(user, newEmailId);
+    const uid = user?.uid;
     console.log(uid);
 
-    const userDocRef = db.collection("users").doc(uid);
-    await userDocRef.update({
-      email: newEmailId
-    });
+    const userRef = doc(db, "users", user?.uid);
+    const userSnapshot = await getDoc(userRef);
 
-    console.log("Email updated");
+    if (userSnapshot.exists()) {
+      const userData = userSnapshot.data();
+      const userEmail = userData.email;
+
+      console.log(userData);
+      console.log("Email found:", userEmail);
+
+      await updateDoc(userRef, {
+        email: newEmailId
+      });
+
+      console.log("New email updated");
+    } else {
+      console.error("User not found");
+    }
+
+    console.log(userRef);
+    console.log("Email updated successfully");
 
   } catch (error) {
     console.error("Could not update email:", error);
   }
 }
 
-updateEmailId("aishwaryak@brightmindenrichment.org", "aishwaryakatkar53@gmail.com");
+// updateEmailId("aishwaryakatkar53@gmail.com");
+
