8641400adbd1a184b0da687f2b287a640ae2bd99
srushtisatardekar <srushtistar10@gmail.com>
2024-03-18 22:57:13 -0700

    changes in srushtiSample

--- src/component/SampleSrushti.js
+++ src/component/SampleSrushti.js
@@ -21,9 +21,9 @@ function SrushtiSample() {
                      By joining the cause, you can contribute to meaningful impact and personal fullfillment.
                  </div>
                  <div className=" mt-10 w-full h-fit justify-start items-start">
-                   <div className="w-full h-fit justify start items-start grid grid-cols-1 lg:grid-cols-2 font-bricolage ">
+                   <div className="w-full h-fit justify start items-start grid grid-cols-2 lg:grid-cols-2 font-bricolage ">
                     
-                    <div className="grow items-start h-full lg:items-start xl:items-start flex-col rounded-t-2xl  lg:rounded-tr-none lg:rounded-tl-2xl lg:rounded-bl-2xl shrink basis-0  px-8 py-4 bg-gradient-to-br from-emerald-300 to-neutral-200 gap-6 flex">
+                    <div className="grow items-start h-full lg:items-start xl:items-start flex-col rounded-tl-2xl rounded-bl-2xl lg:rounded-tr-none lg:rounded-tl-2xl lg:rounded-bl-2xl shrink basis-0  px-8 py-4 bg-gradient-to-br from-emerald-300 to-neutral-200 gap-6 flex">
                       <div className="text-violet-950 text-2xl lg:text-xl font-bricolage font-medium leading-loose text-[40px] font-normal leading-[64px]">
                         580K+ 
                         <br/>
@@ -31,7 +31,7 @@ function SrushtiSample() {
 
                       </div>
                     </div>
-                    <div className="grow items-start h-full lg:items-start xl:items-start flex-col rounded-tr-2xl lg:rounded-tr-2xl lg:rounded-br-2xl lg:rounded-bl-none shrink basis-0 px-8 py-4 bg-gradient-to-br from-purple-200 to-neutral-200 gap-6 flex">
+                    <div className="grow items-start h-full lg:items-start xl:items-start flex-col rounded-tr-2xl rounded-br-2xl lg:rounded-tr-2xl lg:rounded-br-2xl lg:rounded-bl-none shrink basis-0 px-8 py-4 bg-gradient-to-br from-purple-200 to-neutral-200 gap-6 flex">
                       <div className="text-violet-950 text-2xl lg:text-xl font-bricolage font-medium leading-loose text-[40px] font-normal leading-[64px]">
                         24%
                         <br/>
@@ -46,6 +46,9 @@ function SrushtiSample() {
                  <h2 className="text-neutral-800 lg:text-[20px] text-[20px] font-medium font-bricolage lg:leading-[64px] leading-[44px] pb-10 ">
                      Stories
                  </h2>
+                 <div className="bg-[#F5EEFE] rounded-[30px] flex flex-col h-full">
+                  
+                 </div>
 
          
              </div>
