4d1498324d59780e6c2429d2ef9ac6c5019c4273
vi1801 <vrdalwadi18@gmail.com>
2024-04-13 16:33:33 -0700

    updated delete user data extension

--- src/component/UserProfile/DeleteUserExtensionUse.js
+++ src/component/UserProfile/DeleteUserExtensionUse.js
@@ -1,15 +1,24 @@
 import React, { useState } from 'react';
-import firebase from "firebase";
+import { auth } from "../firebase";
 
 const DeleteUserData = () => {
     const [userId, setUserId] = useState("");
+    const [deleteResult, setDeleteResult] = useState("");
 
     const handleDelete = async () => {
-        const extension = firebase.extensions().get("delete-user-data");
-        await extension.call({
-            uid: userId,
-        });
-    
+        try {
+            // getting the extension 
+            const extension = auth.app.extensions.get("delete-user-data");
+            // call the extension with the usere's UID
+            const result = await extension.call({
+                uid: userId
+            });
+            console.log("User data deleted successfully:", result);
+            setDeleteResult("User data deleted successfully.");
+        } catch (error) {
+            console.error("Error deleting user data: ", error);
+            setDeleteResult("Error deleting user data.");
+        }
     };
     /*return (
 
