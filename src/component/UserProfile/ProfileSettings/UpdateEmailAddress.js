8954ce3b441816f61d50848570b31e2477b11bb5
tejas-vivek <tejas.helwatkar@gmail.com>
2024-04-15 13:43:55 -0400

    Added route for updateEmailAddress, created updateEmailAddress page, added links to profileSettings options

---
+++ src/component/UserProfile/ProfileSettings/UpdateEmailAddress.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const UpdateEmailAddress = () => {
+  return (
+    <div className="bg-gradient-to-tr from-[#E4EEEA] from-10% via-[#E4EEEA] via-60% to-[#EAEEB5] to-90% bg-fixed">
+      <div className="relative flex flex-col items-center gap-8 ">
+        <div className=" w-[95%] md:w-[90%] lg:w-[100%] lg:max-w-[864px] xl:max-w-[1120px] mx-2 lg:mx-40 mt-32 mb-16 rounded-2xl bg-[#f7f7f7] text-black "></div>
+      </div>
+    </div>
+  );
+};
+
+export default UpdateEmailAddress;
