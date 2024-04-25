f1b402e3681cd4d13655c58a130d5c10d5dc6223
aniket14thumar <aniket14.thumar@gmail.com>
2024-04-25 09:32:21 -0400

    Individual Community Outreach VisitLogDetails

--- src/component/Community/OutreachVisitLogCard.js
+++ src/component/Community/OutreachVisitLogCard.js
@@ -9,13 +9,13 @@ import userSlots from "../../images/userSlots.png";
 import verifiedImg from "../../images/verified_purple.png";
 
 const OutreachVisitLogCard = ({ visitLogCardData }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        // <div className="bg-[#F5EEFE] rounded-[30px] flex flex-col h-full">
-        <div className="bg-[#F5EEFE] min-w-full max-w-[320px] lg:w-full rounded-[30px] mb-4 flex flex-col justify-between p-6">
-            {console.log(visitLogCardData)}
-            {/* <div className="inline-flex gap-2 items-center px-4 pt-6 py-2">
+  return (
+    // <div className="bg-[#F5EEFE] rounded-[30px] flex flex-col h-full">
+    <div className="bg-[#F5EEFE] min-w-full max-w-[320px] lg:w-full rounded-[30px] mb-4 flex flex-col justify-between p-6">
+      {console.log(visitLogCardData)}
+      {/* <div className="inline-flex gap-2 items-center px-4 pt-6 py-2">
         <img
           src={visitLogCardData.photoUrl || defaultImage}
           className="w-6 h-6 rounded-full"
@@ -23,16 +23,16 @@ const OutreachVisitLogCard = ({ visitLogCardData }) => {
         <div>{visitLogCardData.userName || "not defined"}</div>
       </div> */}
 
-            <div className="inline-flex items-center space-x-2 ">
-                <img
-                    src={visitLogCardData.photoUrl || defaultImage}
-                    className="w-8 h-8 rounded-full"
-                />
-                <div>{visitLogCardData.userName || "not defined"}</div>
-                <img src={verifiedImg} className="w-5 h-5" />
-            </div>
+      <div className="inline-flex items-center space-x-2 ">
+        <img
+          src={visitLogCardData.photoUrl || defaultImage}
+          className="w-8 h-8 rounded-full"
+        />
+        <div>{visitLogCardData.userName || "not defined"}</div>
+        <img src={verifiedImg} className="w-5 h-5" />
+      </div>
 
-            {/* <div className="flex-1 px-4 xl:py-1.5 lg:py-4 py-3">
+      {/* <div className="flex-1 px-4 xl:py-1.5 lg:py-4 py-3">
         <div className="space-y-4">
           <div className="flex flex-col justify-between space-y-3">
             <div className="flex flex-row justify-normal space-x-2">
@@ -66,62 +66,62 @@ const OutreachVisitLogCard = ({ visitLogCardData }) => {
         </div>
       </div> */}
 
-            <div className="my-3 space-y-3 w-full h-full flex flex-col">
-                <div className="flex flex-col justify-between space-y-3">
-                    <div className="flex flex-row justify-normal space-x-2">
-                        <img
-                            className="w-[13px] h-[15px] my-[3px]"
-                            src={date}
-                        />
-                        <div className="font-medium font-dmsans text-[14px] text-[#37168B]">
-                            {visitLogCardData?.eventDate}
-                        </div>
-                    </div>
-                    <div className="flex flex-row justify-normal space-x-2">
-                        <img
-                            className="w-[12px] h-[15px] my-[3px]"
-                            src={locate}
-                        />
-                        <div className="font-medium font-dmsans text-[14px] text-[#37168B]">
-                            {visitLogCardData?.location?.city},{" "}
-                            {visitLogCardData?.location?.state}
-                        </div>
-                    </div>
-                </div>
-                {/* <div className="font-medium text-[20px] font-dmsans">{title}</div> */}
-
-                <div className="font-medium text-[20px] font-dmsans text-[#444746] line-clamp-1">
-                    {visitLogCardData.description || ""}
-                </div>
-
-                <div className="flex flex-row justify-between">
-                    <div className="font-bold text-[14px] font-dmsans text-[#444746] line-clamp-1">
-                        People Helped
-                    </div>
-                    <div className="font-bold text-[14px] font-dmsans text-[#444746] line-clamp-1">
-                        {visitLogCardData?.numberPeopleHelped}
-                    </div>
-                </div>
-
-                <div className="flex flex-row justify-between">
-                    <div className="font-bold text-[14px] font-dmsans text-[#444746] line-clamp-1">
-                        Items Donated
-                    </div>
-                    <div className="font-bold text-[14px] font-dmsans text-[#444746] line-clamp-1">
-                        {visitLogCardData?.itemQty}
-                    </div>
-                </div>
-
-                <div className="inline-flex items-center gap-2 flex-wrap">
-                    {visitLogCardData?.whatGiven.map((item, index) => (
-                        <div className="py-1 px-3 border border-[#C8C8C8] w-fit rounded-xl text-[12px] text-[#444746]">
-                            {item}
-                        </div>
-                    ))}
-                </div>
+      <div className="my-3 space-y-3 w-full h-full flex flex-col">
+        <div className="flex flex-col justify-between space-y-3">
+          <div className="flex flex-row justify-normal space-x-2">
+            <img className="w-[13px] h-[15px] my-[3px]" src={date} />
+            <div className="font-medium font-dmsans text-[14px] text-[#37168B]">
+              {visitLogCardData?.eventDate}
             </div>
+          </div>
+          <div className="flex flex-row justify-normal space-x-2">
+            <img className="w-[12px] h-[15px] my-[3px]" src={locate} />
+            <div className="font-medium font-dmsans text-[14px] text-[#37168B]">
+              {visitLogCardData?.location?.city},{" "}
+              {visitLogCardData?.location?.state}
+            </div>
+          </div>
+        </div>
+        {/* <div className="font-medium text-[20px] font-dmsans">{title}</div> */}
 
-            {/* <div className="pt-2 px-4 pb-6 space-x-2">
+        <div className="font-medium text-[20px] font-dmsans text-[#444746] line-clamp-2">
+          {visitLogCardData.description || ""}
+        </div>
+
+        <div className="flex flex-row justify-between">
+          <div className="font-bold text-[14px] font-dmsans text-[#444746] line-clamp-1">
+            People Helped
+          </div>
+          <div className="font-bold text-[14px] font-dmsans text-[#444746] line-clamp-1">
+            {visitLogCardData?.numberPeopleHelped}
+          </div>
+        </div>
+
+        <div className="flex flex-row justify-between">
+          <div className="font-bold text-[14px] font-dmsans text-[#444746] line-clamp-1">
+            Items Donated
+          </div>
+          <div className="font-bold text-[14px] font-dmsans text-[#444746] line-clamp-1">
+            {visitLogCardData?.itemQty}
+          </div>
+        </div>
+
+        <div className="inline-flex items-center gap-2 flex-wrap">
+          {visitLogCardData?.whatGiven.slice(0, 4).map((item, index) => (
+            <div className="py-1 px-3 border border-[#C8C8C8] w-fit rounded-xl text-[12px] text-[#444746]">
+              {item}
+            </div>
+          ))}
+          {visitLogCardData?.whatGiven.length > 4 && (
+            <div className="text-[12px]">
+              {" "}
+              +{visitLogCardData?.whatGiven.length - 4} more
+            </div>
+          )}
+        </div>
+      </div>
+
+      {/* <div className="pt-2 px-4 pb-6 space-x-2">
         <CustomButton
           label="View Details"
           name="buttonlightsmall"
@@ -132,32 +132,27 @@ const OutreachVisitLogCard = ({ visitLogCardData }) => {
         <CustomButton label="Share" name="buttonsmallborder2" />
       </div> */}
 
-            <div className=" flex flex-col justify-end">
-                <div className="flex items-center justify-between gap-16 my-1">
-                    <div class="group relative">
-                        <CustomButton
-                            label="View Details"
-                            name="buttonlightsmall"
-                            onClick={() => {
-                                navigate(
-                                    `/VisitLogDetails/${visitLogCardData.id}`
-                                );
-                            }}
-                        />
-                    </div>
-                    <div className="flex flex-row space-x-2">
-                        <img
-                            className="w-[20px] h-[14px] my-1"
-                            src={userSlots}
-                        ></img>
-                        <div className="font-normal font-dmsans text-[14px]">
-                            {visitLogCardData?.filledSlots}
-                        </div>
-                    </div>
-                </div>
+      <div className=" flex flex-col justify-end">
+        <div className="flex items-center justify-between gap-16 my-1">
+          <div class="group relative">
+            <CustomButton
+              label="View Details"
+              name="buttonlightsmall"
+              onClick={() => {
+                navigate(`/VisitLogDetails/${visitLogCardData.id}`);
+              }}
+            />
+          </div>
+          <div className="flex flex-row space-x-2">
+            <img className="w-[20px] h-[14px] my-1" src={userSlots}></img>
+            <div className="font-normal font-dmsans text-[14px]">
+              {visitLogCardData?.filledSlots}
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default OutreachVisitLogCard;
