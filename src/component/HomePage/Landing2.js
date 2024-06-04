bd8d77e8c6fc6e700decd9888381aecc19f506f8
Bhawna Singh <singh.bhaw@northeastern.edu>
2024-06-04 18:27:38 -0400

    Landing2 page

---
+++ src/component/HomePage/Landing2.js
@@ -0,0 +1,54 @@
+import React, { forwardRef, useImperativeHandle } from "react";
+import one from "../../images/one.png";
+import two from "../../images/two.png";
+import three from "../../images/three.png";
+import CustomButton from "../Buttons/CustomButton";
+
+const Landing = forwardRef(({ scorllFuntion }, ref) => {
+  const handleOnClick = () => {
+    // Do something in the child component
+    scorllFuntion(); // Call the parent function
+  };
+  useImperativeHandle(ref, () => ({
+    // Expose parent function to parent component
+    callParentFunction: handleOnClick,
+  }));
+  return (
+    <div className="items-center justify-center px-2 py-2 lg:p-24 h-full w-full rounded-2xl  grid  lg:grid-cols-2 gap-3 bg-[#F7F7F7]">
+      <div className="  flex-col">
+        <p className="font-dmsans font-medium text-4xl md:text-[42px] md:leading-[48px]  text-black ">
+          {" "}
+          Support homeless people.{" "}
+          <p className="font-dmsans font-medium text-4xl md:text-[42px] md:leading-[48px] text-black ">
+            Prepare care packages.
+          </p>
+        </p>
+        <p className="font-dmsans font-medium text-4xl md:text-[42px] md:leading-[48px] text-black ">
+          Join outreach.{" "}
+          <p className="font-dmsans font-medium text-4xl md:text-[42px] md:leading-[48px] text-black ">
+            Make a difference.
+          </p>
+        </p>
+        <div className="mt-12 mb-5 p-4">
+          <CustomButton
+            label="Join Community Outreach"
+            name="buttonlight"
+            onClick={handleOnClick}
+          />
+        </div>
+      </div>
+      <div className=" grid grid-cols-1 gap-y-4">
+        <div className=" grid grid-cols-5 gap-x-4">
+          <img src={one} className=" rounded-2xl h-full w-fit col-span-2"></img>
+          <img src={two} className=" rounded-2xl w-fit col-span-3"></img>
+        </div>
+
+        <div className="  col-span-2 ">
+          <img src={three} className="rounded-2xl"></img>
+        </div>
+      </div>
+    </div>
+  );
+});
+
+export default Landing;
