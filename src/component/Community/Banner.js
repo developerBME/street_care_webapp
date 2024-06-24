eadc68761d9cb6cd015c2c2891976f677df447a3
rishabh <rishabh0495@gmail.com>
2024-06-24 18:22:37 -0400

    Community Banner - Rounded border fix

--- src/component/Community/Banner.js
+++ src/component/Community/Banner.js
@@ -40,7 +40,7 @@ function Metrics() {
         {/* Column 3 */}
         <div className="w-1/4 sm:w-1/4 md:w-1/4 lg:w-1/4 lg:px-[28px] lg:py-[20px] md:p-[18px] p-[18px] relative border-[#F2F2F2] lg:text-left md:text-center sm:text-center text-center after:content-[''] after:absolute after:border-[1px] after:border-[#F2F2F2] after:border-solid after:top-[30%] after:right-0 lg:after:h-[60px] md:after:h-[60px] after:h-[40px]">
           {/* Column content */}
-          <p className="font-bold text-violet-800 lg:mb-[24px] md:mb-[10px] sm:mb-[15px text-center]">
+          <p className="font-bold text-violet-800 lg:mb-[24px] md:mb-[10px] sm:mb-[15px] text-center">
             Donated
           </p>
           <p className=" text-center">
@@ -73,8 +73,8 @@ function Banner() {
   return (
     <div className="items-center justify-center h-full w-full rounded-2xl bg-pattern sm:bg-[url('images/community_bg2.png')] bg-[url('images/community_bg4.png')] sm:bg-[left_bottom_-200px] bg-[left_bottom_0] sm:bg-[left_bottom_-100px] bg-no-repeat relative">
       <div className="absolute bottom-0 right-0 z-[9]">
-        <img src={one} alt="" className="hidden sm:block" />
-        <img src={two} alt="" className="block sm:hidden" />
+        <img src={one} alt="" className="hidden sm:block rounded-br-2xl" />
+        <img src={two} alt="" className="block sm:hidden rounded-br-2xl" />
       </div>
       <div className="px-[8px] pt-6 lg:px-24 lg:pt-[15px] md:px-[25px] md:pt-[15px] bg-[right_bottom_-150px] bg-no-repeat relative">
         <div className="lg:items-start md:items-center justify-center h-full w-full grid md:grid-cols-4 lg:grid-cols-5 gap-2">
@@ -87,7 +87,8 @@ function Banner() {
               superhero!
             </p>
             <p className="text-[#181818] lg:text-[24px] leading-8 mt-4 pr-5">
-              Or, know someone in need? Create a help request for others to help.
+              Or, know someone in need? Create a help request for others to
+              help.
             </p>
           </div>
           <div className="lg:col-span-3 md:col-span-2 z-10">
