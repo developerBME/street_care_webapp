14ea4af6622129b5d7c31df80196d08a0a70b5ae
Bhawna Singh <singh.bhaw@northeastern.edu>
2024-07-12 16:26:28 -0400

    Added buttons to NoDisplayData

--- src/component/UserProfile/NoDisplayData.js
+++ src/component/UserProfile/NoDisplayData.js
@@ -1,6 +1,7 @@
 import React from "react";
 import DocVec from "../../images/DocVec.svg";
 import { useNavigate } from "react-router-dom"; 
+import CustomButton from "../Buttons/CustomButton";
 
 
 function NoDisplayData({name,label}){
@@ -14,7 +15,23 @@ function NoDisplayData({name,label}){
                     />
                 </div>
                 <div className="h-fit flex flex-col justify-center items-center gap-2">
-                    <div className="text-center text-neutral-900 text-[28px] font-medium font-['DM Sans'] leading-9">{label}</div>
+                    <div className="text-center text-neutral-900 text-[28px] font-medium font-['DM Sans'] leading-9 mt-9">{label}</div>
+                </div>
+                <div className="flex justify-center items-start gap-4 mt-9">
+                    <CustomButton 
+                        label="Document Personal Outreach"
+                        name="buttondefault"
+                        onClick={() => {
+                            window.scrollTo(0, 0);
+                        }}
+                    />
+                    <CustomButton 
+                        label="Explore Outreach Events"
+                        name="buttonlight"
+                        onClick={() => {
+                            window.scrollTo(0, 0);
+                    }}
+                    />
                 </div>
             </div>
         );
@@ -28,7 +45,23 @@ function NoDisplayData({name,label}){
                     />
                 </div>
                 <div className="h-fit flex flex-col justify-center items-center gap-2">
-                    <div className="text-center text-neutral-900 text-[28px] font-medium font-['DM Sans'] leading-9">{label}</div>
+                    <div className="text-center text-neutral-900 text-[28px] font-medium font-['DM Sans'] leading-9 mt-9">{label}</div>
+                </div>
+                <div className="flex justify-center items-start gap-4 mt-9">
+                    <CustomButton 
+                        label="Document visit logs"
+                        name="buttondefault"
+                        onClick={() => {
+                            window.scrollTo(0, 0);
+                        }}
+                    />
+                    <CustomButton 
+                        label="Explore visit logs"
+                        name="buttonlight"
+                        onClick={() => {
+                            window.scrollTo(0, 0);
+                    }}
+                    />
                 </div>
             </div>
         );
@@ -42,7 +75,23 @@ function NoDisplayData({name,label}){
                     />
                 </div>
                 <div className="h-fit flex flex-col justify-center items-center gap-2">
-                    <div className="text-center text-neutral-900 text-[28px] font-medium font-['DM Sans'] leading-9">{label}</div>
+                    <div className="text-center text-neutral-900 text-[28px] font-medium font-['DM Sans'] leading-9 mt-9">{label}</div>
+                </div>
+                <div className="flex justify-center items-start gap-4 mt-9">
+                    <CustomButton 
+                        label="Document Help Requests"
+                        name="buttondefault"
+                        onClick={() => {
+                            window.scrollTo(0, 0);
+                        }}
+                    />
+                    <CustomButton 
+                        label="Explore Help Requests"
+                        name="buttonlight"
+                        onClick={() => {
+                            window.scrollTo(0, 0);
+                    }}
+                    />
                 </div>
             </div>
         );
