a5c7bd7383583435543ada0569b666eb5acab954
laxmiSindhuja7 <laxmisindhuja555@example.com>
2024-07-17 18:25:18 -0400

    admin panel protected route changes

--- src/component/ProtectedAdminRoute.js
+++ src/component/ProtectedAdminRoute.js
@@ -4,24 +4,27 @@ import { fetchAdminVerification } from "./DecodeService";
 
 export const ProtectedAdminRoute = ({ user, loading }) => {
 
-  const [isUserVerified, setIsUserVerified] = useState(true);
+  const [isUserVerified, setIsUserVerified] = useState(false);
+  const [fetchComplete, setFetchComplete] = useState(false); // to check if the AdminVerification call is complete
 
-  useEffect(() => {
-    const fetchUserVerification = async () => {
+  const fetchUserVerification = async () => {
       if (user && user.accessToken) {
         try {
           const verificationDetails = await fetchAdminVerification(user.accessToken);
           console.log(verificationDetails)
           setIsUserVerified(verificationDetails?.flag);
+          setFetchComplete(true);
         } catch (error) {
-          console.error('Error fetching verification details:', error);
+          setFetchComplete(true);
         }
       }
-    };
+  };
+
+  useEffect(() => {
     fetchUserVerification();
   });
-
-  if (loading) return null;
+  
+  if (loading || !fetchComplete) return null; 
 
   if (!user || !Object.keys(user).length) return <Navigate to="/login" />;
 
