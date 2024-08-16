1f164d09ca8293af2613f251b2d96a5b1ec63af3
Abhishek L S <abhishekk.l.s1998@gmail.com>
2024-08-16 14:19:11 -0400

    Pulled the feature branch

--- src/component/Community/OutreachEventCard.js
+++ src/component/Community/OutreachEventCard.js
@@ -44,10 +44,7 @@ const OutreachEventCard = ({
   };
 
   return (
-    <div className="bg-[#F5EEFE] min-w-full max-w-[320px] lg:w-full rounded-[30px] mb-4 flex flex-col justify-between p-6 cursor-pointer"
-    onClick={() => {
-      navigate(`/outreachsignup/${id}`);
-    }} >
+    <div className="bg-[#F5EEFE] min-w-full max-w-[320px] lg:w-full rounded-[30px] mb-4 flex flex-col justify-between p-6">
       {!isProfilePage && !isPastEvent ? (
         <div className="inline-flex items-center space-x-2 ">
           <img
@@ -147,16 +144,6 @@ const OutreachEventCard = ({
             {isProfilePage || label2 === "EDIT" ? (
               <div class="group relative">
                 <CustomButton
-                  label="View"
-                  name="buttonlight"
-                  onClick={() => {
-                    navigate(`/outreachsignup/${id}`, {
-                      state: { label: "EDIT" },
-                    });
-                  }}
-                ></CustomButton>
-
-                {/* <CustomButton
                   label="Edit"
                   name="buttonlight"
                   onClick={handleEditClick}
@@ -175,26 +162,21 @@ const OutreachEventCard = ({
                     eventDate={eventDate}
                     location={location}
                   />
-                )} */}
+                )}
               </div>
             ) : (
-              <div className="flex flex-row">
-                {/* <div className="group relative mr-2">
-                  <CustomButton
-                    label={label2}
-                    name="buttonlight"
-                    onClick={() => {
-                      navigate(`/outreachsignup/${id}`);
-                    }}
-                  />
-                </div> */}
-                
+              <div className="group relative">
+                <CustomButton
+                  label={label2}
+                  name="buttonlight"
+                  onClick={() => {
+                    navigate(`/outreachsignup/${id}`);
+                  }}
+                />
               </div>
             )}
 
-
-              {/* This code is for adding number of volunteer ppl value to show up. */}
-            {/* {!isProfilePage ? (
+            {!isProfilePage ? (
               <div className="flex flex-row space-x-2">
                 <img
                   alt=""
@@ -207,7 +189,7 @@ const OutreachEventCard = ({
               </div>
             ) : (
               <div></div>
-            )} */}
+            )}
           </div>
         </div>
       ) : (
