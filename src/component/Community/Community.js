15ed92b429dea2679fae0ce3a20c459bc3d78781
Samkit Shah <imsamkit002@gmail.com>
2024-10-18 18:22:16 -0500

    formatDate and commentedHelpRequest

--- src/component/Community/Community.js
+++ src/component/Community/Community.js
@@ -38,9 +38,9 @@ function Community() {
           <CommunityVisitLog />
         </div> }
 
-        <div className="w-[95%] md:w-[90%] lg:w-[100%] lg:max-w-[864px] xl:max-w-[1120px] mx-2 mb-8 lg:mx-40 mt-8 rounded-2xl bg-white text-black">
+        {/* <div className="w-[95%] md:w-[90%] lg:w-[100%] lg:max-w-[864px] xl:max-w-[1120px] mx-2 mb-8 lg:mx-40 mt-8 rounded-2xl bg-white text-black">
           <HelpRequest updateHelpRequests={updateHelpRequests} />
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- src/component/UserProfile/Profile.js
+++ src/component/UserProfile/Profile.js
@@ -205,11 +205,11 @@ function Profile() {
 
         {/* Created Help Request section */}
 
-        <div className="  w-[95%] md:w-[90%] lg:w-[80%] mx-2 lg:mx-40 mt-8 rounded-2xl bg-white text-black mb-10">
+        {/* <div className="  w-[95%] md:w-[90%] lg:w-[80%] mx-2 lg:mx-40 mt-8 rounded-2xl bg-white text-black mb-10">
           <div className="flex flex-col gap-4 md:px-12 md:py-16 lg:gap-14 lg:p-24 pl-8 pt-4 pb-4 pr-8">
             <CreatedHelpRequests />
           </div>{" "}
-        </div>
+        </div> */}
       </div>
       {
         <Modal open={!!selectedEvent}>
