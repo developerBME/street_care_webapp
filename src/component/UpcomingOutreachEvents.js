56c1dfde0dffd5805decb243870d8eecd0ab40ca
developerBME <144156293+developerBME@users.noreply.github.com>
2024-12-27 19:44:24 -0600

    Update UpcomingOutreachEvents.js
    changed street care Leader back to Internal Member

--- src/component/UpcomingOutreachEvents.js
+++ src/component/UpcomingOutreachEvents.js
@@ -85,7 +85,7 @@ const UpcomingOutreachEvents = ({
                             className="w-6 h-6"
                         />
                         <span className="text-sm font-medium text-gray-700">
-                            Street Care Leader
+                            Internal Member
                         </span>
                     </div>
                     {/* Other */}
