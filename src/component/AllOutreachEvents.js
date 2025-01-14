77078c09a6ea0d256c9d156af5c6129397aea8cf
aditya-12 <adityakaushik2011@gmail.com>
2025-01-14 17:19:52 -0600

    Updated chapter member and fixed Signup functionality

--- src/component/AllOutreachEvents.js
+++ src/component/AllOutreachEvents.js
@@ -375,15 +375,15 @@ const AllOutreachEvents = ({ loggedIn }) => {
                 Chapter Leader
               </span>
             </div>
-            {/* Chapter Member */}
+            {/* Streetcare Hub Leader */}
             <div className="flex items-center space-x-2">
               <img
                 src={verifiedPurple}
-                alt="Chapter Member"
+                alt="Streetcare Hub Leader"
                 className="w-6 h-6"
               />
               <span className="text-sm font-medium text-gray-700">
-                Chapter Member
+              Streetcare Hub Leader
               </span>
             </div>
             {/* Internal Member */}
@@ -397,14 +397,14 @@ const AllOutreachEvents = ({ loggedIn }) => {
                 Internal Member
               </span>
             </div>
-            {/* Other */}
+            {/* Account holder */}
             <div className="flex items-center space-x-2">
               <img
                 src={verifiedYellow}
-                alt="Other"
+                alt="Account holder"
                 className="w-6 h-6"
               />
-              <span className="text-sm font-medium text-gray-700">Other</span>
+              <span className="text-sm font-medium text-gray-700">Account holder</span>
             </div>
           </div>
 
--- src/component/HomePage/HomePageVisitlog.js
+++ src/component/HomePage/HomePageVisitlog.js
@@ -118,15 +118,15 @@ const HomePageVisitlog = () => {
                     Chapter Leader
                   </span>
                 </div>
-                {/* Chapter Member */}
+                {/* Streetcare Hub Leader */}
                 <div className="flex items-center space-x-2">
                   <img
                     src={verifiedPurple}
-                    alt="Chapter Member"
+                    alt="Streetcare Hub Leader"
                     className="w-6 h-6"
                   />
                   <span className="text-sm font-medium text-gray-700">
-                    Chapter Member
+                  Streetcare Hub Leader
                   </span>
                 </div>
                 {/* Internal Member */}
@@ -140,14 +140,14 @@ const HomePageVisitlog = () => {
                     Internal Member
                   </span>
                 </div>
-                {/* Other */}
+                {/* Account holder */}
                 <div className="flex items-center space-x-2">
                   <img
                     src={verifiedYellow}
-                    alt="Other"
+                    alt="Account holder"
                     className="w-6 h-6"
                   />
-                  <span className="text-sm font-medium text-gray-700">Other</span>
+                  <span className="text-sm font-medium text-gray-700">Account holder</span>
                 </div>
               </div>
           </div>
