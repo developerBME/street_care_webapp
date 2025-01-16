3c2f08106f57e540ecb6096f97d42329cddcb726
ShlokaSingh <shloka.singh@gmail.com>
2025-01-15 19:22:42 -0500

    User type name change dev

--- src/component/Community/Form.js
+++ src/component/Community/Form.js
@@ -2,21 +2,31 @@ import React, { useRef, useState, useEffect } from "react";
 
 import Chip from "../Community/Chip";
 import arrowDown from "../../images/arrowDown.png";
-import { doc, updateDoc, addDoc, collection, getDoc, query } from "firebase/firestore";
+import {
+  doc,
+  updateDoc,
+  addDoc,
+  collection,
+  getDoc,
+  query,
+} from "firebase/firestore";
 import { db } from "../firebase";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import errorImg from "../../images/error.png";
 import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from "react-datepicker";
-import { Timestamp,where,getDocs } from "firebase/firestore";
-import { checkString, checkNumber, checkPhoneNumber } from "../helper/validator";
+import { Timestamp, where, getDocs } from "firebase/firestore";
+import {
+  checkString,
+  checkNumber,
+  checkPhoneNumber,
+} from "../helper/validator";
 import { UpdateDisabledRounded } from "@mui/icons-material";
 import CreateOutreachModal from "./CreateOutreachModal";
 import { fetchHelpReqById } from "../HelpRequestService";
 import { emailConfirmation } from "../EmailService";
 import { Link } from "react-router-dom";
 import { fetchUserTypeDetails } from "../EventCardService";
-
 
 const chipList = [
   "Childcare",
@@ -174,7 +184,7 @@ const Form = (hrid) => {
     if (helpDetails.title) {
       nameRef.current.value = helpDetails.title;
     }
-    if(helpDetails.contactNumber){
+    if (helpDetails.contactNumber) {
       contactRef.current.value = helpDetails.contactNumber;
     }
     if (helpDetails?.location?.street) {
@@ -207,9 +217,12 @@ const Form = (hrid) => {
         // true if redirected from help request and false for organic outreach event.
         const isHelpReqFlow = !(typeof hrid.hrid == "undefined");
         const userDetails = await fetchUserTypeDetails(fAuth.currentUser.uid);
-        let statusValue = 'pending'
-        if(userDetails.type == 'Chapter Leader' || userDetails.type == 'Internal Member') {
-          statusValue = 'approved'
+        let statusValue = "pending";
+        if (
+          userDetails.type == "Chapter Leader" ||
+          userDetails.type == "Streetcare Hub Leader"
+        ) {
+          statusValue = "approved";
         }
         try {
           let obj = {
@@ -241,14 +254,13 @@ const Form = (hrid) => {
           // const eventRef = collection(db, "outreachEvents"); //change back to this line in dev branch
           const eventRef = collection(db, "outreachEventsDev");
 
-
           async function postDoc(ref, obj) {
             const docRef = await addDoc(ref, obj);
             return docRef.id;
           }
           const ack = await postDoc(eventRef, obj);
 
-          //added outreach to user collection 
+          //added outreach to user collection
           const userQuery = query(
             collection(db, USERS_COLLECTION),
             where("uid", "==", fAuth?.currentUser?.uid)
@@ -567,7 +579,7 @@ const Form = (hrid) => {
       autoCompleteRef.current,
       {
         types: ["address"],
-        componentRestrictions: { country: ["us","ca"] },
+        componentRestrictions: { country: ["us", "ca"] },
       }
     );
 
@@ -690,11 +702,12 @@ const Form = (hrid) => {
               {error.contactError && (
                 <div className="inline-flex items-center">
                   <img alt="" src={errorImg} className="w-3 h-3" />
-                  <p className="text-red-600 text-xs mx-1">{error.contactError}</p>
+                  <p className="text-red-600 text-xs mx-1">
+                    {error.contactError}
+                  </p>
                 </div>
               )}
             </div>
-
 
             <div className="space-y-1.5">
               <p className="font-semibold font-['Inter'] text-[15px]">
@@ -724,8 +737,8 @@ const Form = (hrid) => {
               </p>
               <input
                 type="number"
-                min='0'
-                step='1'
+                min="0"
+                step="1"
                 className="h-12 px-4 w-full block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 "
                 id="max-cap"
                 ref={maxCapRef}
--- src/component/UserProfile/PersonalOutForm.js
+++ src/component/UserProfile/PersonalOutForm.js
@@ -25,9 +25,9 @@ import { fetchPersonalVisitLogById } from "../VisitLogCardService";
 import UpdateVisitLogConfirmationModal from "./UpdateVisitLogConfirmationModal";
 import DatePicker from "react-datepicker";
 import { Timestamp } from "firebase/firestore";
-import InfoIcon from '@mui/icons-material/Info';
+import InfoIcon from "@mui/icons-material/Info";
 // import { IconButton } from "@mui/material";
-import {Tooltip, IconButton} from '@mui/material';
+import { Tooltip, IconButton } from "@mui/material";
 import { fetchUserTypeDetails } from "../EventCardService";
 
 const USERS_COLLECTION = "users";
@@ -178,7 +178,7 @@ function PersonalOutForm() {
 
   const handleDateTimeChange = (date) => {
     updateErrorState("dateTimeError", "");
-    console.log('Selected date:', date);
+    console.log("Selected date:", date);
   };
 
   const handleCityChange = (e) => {
@@ -463,9 +463,12 @@ function PersonalOutForm() {
     }
 
     const userDetails = await fetchUserTypeDetails(fAuth.currentUser.uid);
-    let statusValue = 'pending'
-    if(userDetails.type == 'Chapter Leader' || userDetails.type == 'Internal Member') {
-      statusValue = 'approved'
+    let statusValue = "pending";
+    if (
+      userDetails.type == "Chapter Leader" ||
+      userDetails.type == "Streetcare Hub Leader"
+    ) {
+      statusValue = "approved";
     }
 
     let obj = {
@@ -484,7 +487,7 @@ function PersonalOutForm() {
       street: street,
       dateTime: Timestamp.fromDate(dateTime),
       public: isPublic,
-      status: statusValue
+      status: statusValue,
     };
     console.log(obj);
 
@@ -502,7 +505,7 @@ function PersonalOutForm() {
 
     try {
       console.log("Sending email...");
-//       const logRef = collection(db, "personalVisitLog");
+      //       const logRef = collection(db, "personalVisitLog");
       const logRef = collection(db, "visitLogWebProd"); //change back to this line in dev branch
       const docRef = await addDoc(logRef, obj);
       if (docRef.id) {
@@ -573,7 +576,7 @@ function PersonalOutForm() {
       autoCompleteRef.current,
       {
         types: ["address"],
-        componentRestrictions: { country: ["us","ca"] },
+        componentRestrictions: { country: ["us", "ca"] },
       }
     );
 
@@ -815,14 +818,25 @@ function PersonalOutForm() {
     );
   }, []);
 
-  const toolTipContent=(
+  const toolTipContent = (
     <div>
       Mention here the total quantity of items like 5, 12, 20..
-        <ul className="list-disc list-inside">
-          <li>Item: A single, standalone object. Count those individually (e.g., a book, a shirt, a toy, a food can). </li>
-          <li>Collection: Multiple similar items grouped together that cannot be counted. Count them as 1 item (e.g. 1 bag of toys, 1 bag of Legos, 1 box of pins). </li>
-          <li>Bulk Materials:  For materials like fabric, yarn, or crafting supplies,  note the number of pieces (e.g., 1 piece of 5 yards of fabric, 1 roll of wool). </li>
-        </ul>
+      <ul className="list-disc list-inside">
+        <li>
+          Item: A single, standalone object. Count those individually (e.g., a
+          book, a shirt, a toy, a food can).{" "}
+        </li>
+        <li>
+          Collection: Multiple similar items grouped together that cannot be
+          counted. Count them as 1 item (e.g. 1 bag of toys, 1 bag of Legos, 1
+          box of pins).{" "}
+        </li>
+        <li>
+          Bulk Materials: For materials like fabric, yarn, or crafting supplies,
+          note the number of pieces (e.g., 1 piece of 5 yards of fabric, 1 roll
+          of wool).{" "}
+        </li>
+      </ul>
     </div>
   );
 
@@ -1401,11 +1415,13 @@ function PersonalOutForm() {
                               wrapperClassName="w-full"
                               customInput={
                                 <CustomInput
-                                id="date"
-                                className={`h-12 px-4 w-full block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 ${
-                                  error.dateTimeError !== "" ? "ring-red-500" : "ring-gray-300"
-                                }`}
-                                ref={dateTimeRef}
+                                  id="date"
+                                  className={`h-12 px-4 w-full block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 ${
+                                    error.dateTimeError !== ""
+                                      ? "ring-red-500"
+                                      : "ring-gray-300"
+                                  }`}
+                                  ref={dateTimeRef}
                                 />
                               }
                             />
@@ -1472,7 +1488,7 @@ function PersonalOutForm() {
                       Total number of participants*
                       <Tooltip title={toolTipContent} placement="right" arrow>
                         <IconButton>
-                          <InfoIcon/>
+                          <InfoIcon />
                         </IconButton>
                       </Tooltip>
                     </div>
@@ -1873,14 +1889,19 @@ function PersonalOutForm() {
                               showTimeSelect
                               timeFormat="HH:mm" // Adjust time format as needed
                               dateFormat="dd/MM/yyyy HH:mm" // Adjust date format to include time
-                              minDate={new Date(new Date().getTime() + 24 * 60 * 60 * 1000)}
+                              minDate={
+                                new Date(
+                                  new Date().getTime() + 24 * 60 * 60 * 1000
+                                )
+                              }
                               wrapperClassName="w-full"
                               customInput={
                                 <CustomInput
                                   id="date"
                                   className={`h-12 px-4 w-full block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 ${
-                                    error.dateTimeError  
-                            !== "" ? "ring-red-500" : "ring-gray-300"
+                                    error.dateTimeError !== ""
+                                      ? "ring-red-500"
+                                      : "ring-gray-300"
                                   }`}
                                   ref={dateTimeRef}
                                 />
