bc0d2ec9e50c9a904b8cf04647e3bda6420bbc47
ShlokaSingh <shloka.singh@gmail.com>
2025-01-15 19:44:23 -0500

    Chapter Member

--- src/component/PastOutreachEvents.js
+++ src/component/PastOutreachEvents.js
@@ -52,15 +52,15 @@ const PastOutreachEvents = ({ events, isLoading, isError }) => {
               Chapter Leader
             </span>
           </div>
-          {/* Streetcare Member */}
+          {/* Chapter Member */}
           <div className="flex items-center space-x-2">
             <img
               src={verifiedPurple}
-              alt="Streetcare Member"
+              alt="Chapter Member"
               className="w-6 h-6"
             />
             <span className="text-sm font-medium text-gray-700">
-              Streetcare Member
+              Chapter Member
             </span>
           </div>
           {/* Streetcare Hub Leader */}
