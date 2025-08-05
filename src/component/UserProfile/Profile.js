5dbf30b054283992284726201662ce4dc814c420
Rahul Chandak <rahulschandak2@gmail.com>
2025-08-05 18:25:02 -0400

    fixing infinite loop in profile page

--- src/component/UserProfile/Profile.js
+++ src/component/UserProfile/Profile.js
@@ -107,10 +107,6 @@ function Profile() {
     fetchData();
   }, [triggerEffect]);
 
-  useEffect(() => {
-    fetchData();
-  }, [likedEvents]);
-
   return (
     <div className="bg-gradient-to-tr from-[#E4EEEA] from-10% via-[#E4EEEA] via-60% to-[#EAEEB5] to-90% bg-fixed">
       <div className="relative flex flex-col items-center ">
